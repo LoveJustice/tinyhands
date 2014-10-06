7eca8bac258eea869fc70bfdd1cadd4e56436131
Ben Duggan <benaduggan@gmail.com>
2014-10-06 18:11:35 -0400

    template stuff and things

--- dataentry/alert_checkers.py
+++ dataentry/alert_checkers.py
@@ -1,34 +1,34 @@
+import ipdb
 from accounts.models import Alert
 
 
 class VIFAlertChecker(object):
-
     def __init__(self, form, inlines):
         self.vif = form
         self.inlines = inlines
 
     def check_them(self):
-        self.fir_filed_against()
-        self.dofe_complaint_against()
+        self.fir_and_dofe_against()
         self.ten_or_more_case_points()
 
-    def fir_filed_against(self):
+    def fir_and_dofe_against(self):
         """
-        - Any time a VIF is submitted with the box checked for "FIR filed against __" on question 8.1. E-mail should
+        - Any time a VIF is submitted with the box checked for "FIR filed against __" or for "DoFE complaint against __" on question 8.1. E-mail should
         include VIF number, which type of case has been filed and the name of the person it has been filed against.
         """
+        fir = self.vif.cleaned_data.get("legal_action_against_traffickers_fir_filed")
+        fir_value = self.vif.cleaned_data.get("legal_action_fir_against_value")
+        dofe = self.vif.cleaned_data.get("legal_action_against_traffickers_dofe_complaint")
+        dofe_value = self.vif.cleaned_data.get("legal_action_dofe_against_value")
+        points = self.vif.instance.calculate_strength_of_case_points()
 
-        if self.vif.cleaned_data.get("legal_action_against_traffickers_fir_filed") and self.vif.cleaned_data.get("legal_action_fir_against_value") != '':
-            Alert.alert_objects.send_alert("fir filed against", context={"vif": self.vif.instance})
+        if (fir and fir_value != '') and (dofe and dofe_value != ''):
+            Alert.alert_objects.send_alert("fir and dofe against", context={"vif": self.vif.instance, "both": True, "points": points, "fir_value": fir_value, "dofe_value": dofe_value})
+        if fir and fir_value != '':
+            Alert.alert_objects.send_alert("fir and dofe against", context={"vif": self.vif.instance, "fir": True, "fir_value": fir_value, "points": points})
+        if dofe and dofe_value != '':
+            Alert.alert_objects.send_alert("fir and dofe against", context={"vif": self.vif.instance, "dofe": True, "points": points, "dofe_value": dofe_value})
 
-    def dofe_complaint_against(self):
-        """
-        Any time a VIF is submitted with the box checked for "DoFE complaint against __" on question 8.1. E-mail should
-        include VIF number, which type of case has been filed and the name of the person it has been filed against.
-        """
-
-        if self.vif.cleaned_data.get("legal_action_against_traffickers_dofe_complaint") and self.vif.cleaned_data.get("legal_action_dofe_against_value") != '':
-            Alert.alert_objects.send_alert("dofe complaint against", context={"vif": self.vif.instance})
 
     def ten_or_more_case_points(self):
         """
@@ -37,13 +37,13 @@ class VIFAlertChecker(object):
         """
 
         if self.vif.instance.calculate_strength_of_case_points() > 10:
-            Alert.alert_objects.send_alert("strength of case", context={"vif": self.vif.instance, "points" : self.vif.instance.calculate_strength_of_case_points() })
+            Alert.alert_objects.send_alert("strength of case",
+                                           context={"vif": self.vif.instance, "points": self.vif.instance.calculate_strength_of_case_points()})
 
         pass
 
 
 class IRFAlertChecker(object):
-
     def __init__(self, form, inlines):
         self.irf = form
         self.interceptees = inlines[0]
@@ -64,17 +64,31 @@ class IRFAlertChecker(object):
         """
         Email Alerts to Investigators:
             Any time there is photo of a trafficker on the IRF and the response to question 9.7 is a 4 or a 5
-
             OR any time there is a photo of a trafficker and the Red Flag points calculated by the computer is 400 or
             higher. E-mail should include IRF number, traffickers name, photo, and the reason for the alert.
         """
         trafficker_list = []
         for person in self.interceptees:
-            if person.cleaned_data.get("kind")=='t' and person.cleaned_data.get('photo') not in [None,'']:
+            if person.cleaned_data.get("kind") == 't' and person.cleaned_data.get('photo') not in [None, '']:
                 trafficker_list.append(person.instance)
 
+        trafficker_in_custody = self.IRF_data.get("trafficker_taken_into_custody")
+        trafficker_name = ''
+        if trafficker_in_custody != None and int(self.IRF_data.get("trafficker_taken_into_custody")) < len([there for there in self.interceptees.cleaned_data if there]):
+            trafficker_name = self.interceptees.cleaned_data[int(self.IRF_data.get("trafficker_taken_into_custody")) - 1].get("full_name")
 
-        ipdb.set_trace()
+        red_flags = self.irf.instance.calculate_total_red_flags()
+        certainty_points = self.IRF_data.get('how_sure_was_trafficking')
         if len(trafficker_list) > 0:
-            if (self.IRF_data.get('how_sure_was_trafficking') >= 4) or (self.irf.instance.calculate_total_red_flags() >= 400):
-                Alert.alert_objects.send_alert("Identified Trafficker", context={ "irf" : self.irf.instance, "trafficker_list" : trafficker_list })
+            if (certainty_points >= 4) and (red_flags >= 400):
+                Alert.alert_objects.send_alert("Identified Trafficker", context={"irf": self.irf.instance, "trafficker_list": trafficker_list, "both": True,
+                                                                                 "trafficker_in_custody": trafficker_name, "red_flags": red_flags,
+                                                                                 "certainty_points": certainty_points})
+                return
+            if certainty_points >= 4:
+                Alert.alert_objects.send_alert("Identified Trafficker", context={"irf": self.irf.instance, "trafficker_list": trafficker_list, "how_sure": True,
+                                                                                 "trafficker_in_custody": trafficker_name,
+                                                                                 "certainty_points": certainty_points})
+            if red_flags >= 400:
+                Alert.alert_objects.send_alert("Identified Trafficker", context={"irf": self.irf.instance, "trafficker_list": trafficker_list, "flags": True,
+                                                                                 "trafficker_in_custody": trafficker_name, "red_flags": red_flags})
