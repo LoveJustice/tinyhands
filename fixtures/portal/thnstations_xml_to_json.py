529b7780021ce25767b9f3ec944497c2913d3350
Ben Duggan <benaduggan@gmail.com>
2014-10-31 14:38:06 -0400

    made a xml parser to get the border stations out of the kml file that andrew gave to us so that we can drive the map markers with the database

---
+++ fixtures/portal/thnstations_xml_to_json.py
@@ -0,0 +1,26 @@
+import xmltodict
+
+with open("thnstations.kml") as f:
+    xmldict = xmltodict.parse(f.read())
+    outfile = open('border_stations.json', 'w')
+    list_of_placemarks = xmldict['kml']['Document']['Folder']['Placemark']
+    count = 0
+    json_string = "[\n"
+    for station in list_of_placemarks:
+        both = xmldict['kml']['Document']['Folder']['Placemark'][count]['name'].split()
+        name = both[0]  # take the first work which happens to be the city name
+        station_code = both[1][1:-1]  # take the border station code and take off the leading and ending parenthesis
+        longitude = float(xmldict['kml']['Document']['Folder']['Placemark'][count]["LookAt"]['longitude'])
+        latitude = float(xmldict['kml']['Document']['Folder']['Placemark'][count]["LookAt"]['latitude'])
+        json_station = ("{\n"
+                       "    \"pk\": %d,\n"
+                       "    \"name\": \"%s\",\n"
+                       "    \"code\": \"%s\",\n"
+                       "    \"longitude\": %f,\n"
+                       "    \"latitude\": %f\n"
+                       "},\n"
+                       ) % (count, name, station_code, longitude, latitude)
+        count += 1
+        json_string += json_station
+    json_string = json_string[:len(json_string)-2] + "\n]"  # this takes off the last comma and ends the json file
+    outfile.write(json_string)
--- static/portal.js
+++ static/portal.js
@@ -16,6 +16,60 @@ function initialize() {
         suppressInfoWindows: true,
         map: map
     });
+
+    google.maps.event.addListener(kmlLayer, 'click', function(kmlEvent) {
+        var text = kmlEvent.featureData.name;
+        console.log("name");
+        console.log(kmlEvent.featureData.name);
+        console.log("latitude");
+        console.log(kmlEvent.featureData.latitude);
+        console.log("longtitude")
+        console.log(kmlEvent.featureData.LookAt.latitude);
+        showInContentWindow(text);
+        var marker = new google.maps.Marker({
+              position: (kmlEvent.featureData.LookAt.latitude,kmlEvent.featureData.LookAt.longitude),
+              map: map,
+              title: 'my place'
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+    });
+    function showInContentWindow(text) {
+      var sidediv = document.getElementById('content-window');
+      sidediv.innerHTML = text;
+    }
+
+
+      var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+
+
+
+
 }
 
 function resizeMap() {
