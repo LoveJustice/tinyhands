a788668665404c6fa34688c98b02a44583e912e0
Ben Duggan <benaduggan@gmail.com>
2014-11-06 11:34:13 -0500

    cleaned up the code a bit

--- portal/views.py
+++ portal/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core import serializers
+from django.contrib.auth.decorators import login_required
 
 from dataentry.models import BorderStation
 
+@login_required
 def main_dashboard(request):
     return render(request, "portal/main_dashboard.html")
 
+@login_required
 def get_border_stations(request):
     border_stations = serializers.serialize("json", BorderStation.objects.all())
     return HttpResponse(border_stations, content_type="application/json")
--- static/portal.js
+++ static/portal.js
@@ -4,44 +4,60 @@ function initialize() {
       zoom: 8,
       streetViewControl: false
     };
-
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    getBorderStations(map);
+    createVDCOverlay(map);
+}
 
-    function getContentString(borderStation){
-	return '<div>' +
-	    '<h3>'+borderStation.fields.station_name+'</h3>' +
-	    '<p>'+borderStation.fields.station_code+'</p>' +
-	    '</div>';	    
-    }
-
-    $.get("/portal/get_border_stations",function(data,status){
-        console.log(data);
-        var border_stations = data;
-        var infowindow = new google.maps.InfoWindow({});
-	for(station=0;station<data.length;station++){
-            var myLatlng = new google.maps.LatLng(data[station].fields.latitude,data[station].fields.longitude);
-            console.log(myLatlng)
-            var marker = new google.maps.Marker({
-              position: myLatlng,
-              map: map,
-              title: data[station].fields.station_name + " " + data[station].fields.station_code
-            });
-            google.maps.event.addListener(marker, 'click', (function(marker, station) {
-                return function() {
-                    infowindow.close();
-		    infowindow.setContent(getContentString(data[station]));
-                    infowindow.open(map, marker);
-                }
-            })(marker, station));
-        }
-    });
-
+function createVDCOverlay(map){
+    var VDCLayer = new google.maps.FusionTablesLayer({
+            query: {
+              select: 'col13',
+              from: '1r-omWhMz1wzQG3-e55K7dmCetVe3fRWX4Ai4G_U1'
+            },
+            styles: [{
+          polygonOptions: {
+            fillOpacity: 0.2
+          }}],
+          options: {
+            styleId: 2,
+            templateId: 2
+          }});
+        VDCLayer.setMap(map);
 }
 
 function resizeMap() {
     var map_canvas = document.getElementById('map-canvas');
     map_canvas.style.width = window.innerWidth+"px";
     map_canvas.style.height = (window.innerHeight-50)+"px";
+}
+
+function getContentString(borderStation){
+	return '<div>' +
+	    '<h3>'+borderStation.fields.station_name+'</h3>' +
+	    '<p>'+borderStation.fields.station_code+'</p>' +
+	    '</div>';
+}
+
+function getBorderStations(map){
+    $.get("/portal/get_border_stations",function(data,status){
+        var infowindow = new google.maps.InfoWindow({width:400});
+        for(var station=0;station<data.length;station++){
+                var myLatlng = new google.maps.LatLng(data[station].fields.latitude,data[station].fields.longitude);
+                var marker = new google.maps.Marker({
+                  position: myLatlng,
+                  map: map,
+                  title: data[station].fields.station_name + " " + data[station].fields.station_code
+                });
+                google.maps.event.addListener(marker, 'click', (function(marker, station) {
+                    return function() {
+                        infowindow.close();
+                infowindow.setContent(getContentString(data[station]));
+                        infowindow.open(map, marker);
+                    }
+                })(marker, station));
+            }
+    });
 }
 
 $( window ).resize(function() {
