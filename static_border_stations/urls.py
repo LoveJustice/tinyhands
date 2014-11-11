277196045fe8008698847269ca55fda7fc8b42b7
bsell <bsell@cse20105.cs.local>
2014-11-11 14:41:35 -0500

    Adding editing for border station

--- static_border_stations/urls.py
+++ static_border_stations/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from static_border_stations.views import *
 
 urlpatterns = patterns('static_border_stations.views',
-    url(r'^border-stations/create/$', StaticBorderStationsCreateView.as_view() , name='borderstations_create')
+    url(r'^border-stations/create/$', StaticBorderStationsCreateView.as_view() , name='borderstations_create'),
+    url(r'^border-stations/update/(?P<pk>\d+)/$', StaticBorderStationsUpdateView.as_view(), name='borderstations_update'),
 )
