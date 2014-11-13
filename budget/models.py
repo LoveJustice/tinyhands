4646b0ab8ec3347a51d3dbe46333d657382f977b
Ben Duggan <benaduggan@gmail.com>
2014-11-13 16:35:39 -0500

    merged in borderstations and added migrations for budget calculations

--- budget/models.py
+++ budget/models.py
@@ -12,45 +12,84 @@ class BorderStationBudgetCalculation(models.Model):
 
     border_station = models.ForeignKey(BorderStation)
     
-    communication_chair = models.BooleanField('Chair', default=False)
-    communication_manager = models.BooleanField('Manager', default=False)
-    communication_number_of_staff_with_walkie_talkies = models.PositiveIntegerField('# of staff with walkie-talkies', null=True, blank=True)
-    communication_each_staff = models.PositiveIntegerField('each staff', null=True, blank=True)
-    
-    travel_chair_with_bike = models.BooleanField('Chair with Bike', default=False)
-    travel_manager_with_bike = models.BooleanField('Manager with Bike', default=False)
-    travel_number_of_staff_using_bikes = models.PositiveIntegerField('# of staff using bikes', null=True, blank=True)
-    travel_last_months_expense_for_sending_girls_home = models.FloatField()
-    travel_motorbike = models.BooleanField('Motorbike', default=False)
-    travel_motorbike_amount = models.PositiveIntegerField('Motorbike other amount', null=True, blank=True)
-    
-    administration_number_of_intercepts_last_month = models.PositiveIntegerField('# of intercepts last month', null=True, blank=True)
-    administration_number_of_meetings_per_month = models.PositiveIntegerField('# of meetings per month', null=True, blank=True)
-    administration_booth = models.BooleanField('Booth', default=False)
-    administration_registration = models.BooleanField('Registration', default=False)
-    
-    medical_last_months_expense = models.FloatField()
-    
-    miscellaneous_number_of_intercepts_last_month = models.PositiveIntegerField('# of intercepts last month', null=True, blank=True)
+    communication_chair = models.BooleanField(default=False)
+    communication_chair_amount = models.PositiveIntegerField('for chair', default=1000)
+    communication_manager = models.BooleanField(default=False)
+    communication_manager_amount = models.PositiveIntegerField('for manager (if station has manager)', default=1000)
+    communication_number_of_staff_with_walkie_talkies = models.PositiveIntegerField('# of staff with walkie-talkies', default=0)
+    communication_number_of_staff_with_walkie_talkies_multiplier = models.PositiveIntegerField(default=100)
+    communication_each_staff = models.PositiveIntegerField('each staff', default=0)
+    communication_each_staff_multiplier = models.PositiveIntegerField(default=300)
 
-    shelter_rent = models.FloatField()
-    shelter_water = models.FloatField()
-    shelter_electricity = models.FloatField()
+
+    travel_chair_with_bike = models.BooleanField(default=False)
+    travel_chair_with_bike_amount = models.PositiveIntegerField('for chair (if has bike)', default=2000)
+    travel_manager_with_bike = models.BooleanField(default=False)
+    travel_manager_with_bike_amount = models.PositiveIntegerField('for manager (if has bike)', default=2000)
+    travel_number_of_staff_using_bikes = models.PositiveIntegerField('# of staff using bikes', default=0)
+    travel_number_of_staff_using_bikes_multiplier = models.PositiveIntegerField(default=1000)
+    travel_last_months_expense_for_sending_girls_home = models.PositiveIntegerField(default=0)
+    travel_motorbike = models.BooleanField('Motorbike', default=False)
+    travel_motorbike_amount = models.PositiveIntegerField(default=60000)
+
+
+    administration_number_of_intercepts_last_month = models.PositiveIntegerField('# of intercepts last month', default=0)
+    administration_number_of_intercepts_last_month_multiplier = models.PositiveIntegerField(default=20)
+    administration_number_of_intercepts_last_month_adder = models.PositiveIntegerField(default=1000)
+    administration_number_of_meetings_per_month = models.PositiveIntegerField('# of meetings per month', default=0)
+    administration_number_of_meetings_per_month_multiplier = models.PositiveIntegerField(default=600)
+    administration_booth = models.BooleanField('Booth', default=False)
+    administration_booth_amount = models.PositiveIntegerField(default=30000)
+    administration_registration = models.BooleanField('Registration', default=False)
+    administration_registration_amount = models.PositiveIntegerField(default=2000)
+
+
+    medical_last_months_expense = models.PositiveIntegerField("Last month's medical expense", default=0)
+
+
+    miscellaneous_number_of_intercepts_last_month = models.PositiveIntegerField('# of intercepts last month', default=0)
+    miscellaneous_number_of_intercepts_last_month_multiplier = models.PositiveIntegerField(default=300)
+
+
+    shelter_rent = models.PositiveIntegerField(default=0)
+    shelter_water = models.PositiveIntegerField(default=0)
+    shelter_electricity = models.PositiveIntegerField(default=0)
     shelter_shelter_startup = models.BooleanField('Shelter Startup', default=False)
+    shelter_shelter_startup_amount = models.PositiveIntegerField(default=71800)
     shelter_shelter_two = models.BooleanField('Shelter 2', default=False)
-    
-    food_and_gas_number_of_intercepted_girls = models.PositiveIntegerField('# of intercepted girls', null=True, blank=True)
-    food_and_gas_number_of_limbo_girls = models.PositiveIntegerField('# of limbo girls', null=True, blank=True)
-    food_and_gas_number_of_days = models.PositiveIntegerField('# of days', null=True, blank=True)
-    
+    shelter_shelter_two_amount = models.PositiveIntegerField(default=36800)
+
+
+    food_and_gas_number_of_intercepted_girls = models.PositiveIntegerField('# of intercepted girls', default=0)
+    food_and_gas_number_of_intercepted_girls_multiplier_before = models.PositiveIntegerField(default=100)
+    food_and_gas_number_of_intercepted_girls_multiplier_after = models.PositiveIntegerField(default=3)
+    food_and_gas_limbo_girls_multiplier = models.PositiveIntegerField(default=100)
+    food_and_gas_number_of_limbo_girls = models.PositiveIntegerField('# of limbo girls', default=0)
+    food_and_gas_number_of_days = models.PositiveIntegerField('# of days', default=0)
+
+
     awareness_contact_cards = models.BooleanField('Contact Cards', default=False)
-    awareness_awareness_party = models.FloatField()
-    awareness_sign_boards = models.FloatField()
+    awareness_contact_cards_boolean_amount = models.PositiveIntegerField(default=4000)
+    awareness_contact_cards_amount = models.PositiveIntegerField(default=0)
+    awareness_awareness_party_boolean = models.BooleanField("Awareness Party", default=False)
+    awareness_awareness_party = models.PositiveIntegerField(default=0)
+    awareness_sign_boards_boolean = models.BooleanField("Sign Boards", default=False)
+    awareness_sign_boards = models.PositiveIntegerField(default=0)
+
+
+    supplies_walkie_talkies_boolean = models.BooleanField('Walkie-talkies', default=False)
+    supplies_walkie_talkies_amount = models.PositiveIntegerField(default=0)
+    supplies_recorders_boolean = models.BooleanField('Recorders', default=False)
+    supplies_recorders_amount = models.PositiveIntegerField(default=0)
+    supplies_binoculars_boolean = models.BooleanField('Binoculars', default=False)
+    supplies_binoculars_amount = models.PositiveIntegerField(default=0)
+    supplies_flashlights_boolean = models.BooleanField('Flashlights', default=False)
+    supplies_flashlights_amount = models.PositiveIntegerField(default=0)
 
 
 class OtherBudgetItemCost(models.Model):
-    name = models.CharField(max_length=255, blank=False)
-    cost = models.FloatField()
+    name = models.CharField(max_length=255)
+    cost = models.PositiveIntegerField()
     
     BUDGET_FORM_SECTION_CHOICES = [
         (1, 'Travel'),
