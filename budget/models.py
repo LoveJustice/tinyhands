53244d71eca19b619a00a0ad5c4d1aada1a133ef
Ben Duggan <benaduggan@gmail.com>
2014-11-13 15:30:40 -0500

    progressed models

--- budget/models.py
+++ budget/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from accounts.models import Account
 from dataentry.models import BorderStation
 
-class BorderStationBudgetCalculationForm(models.Model):
-    form_entered_by = models.ForeignKey(Account)
-    date_created = models.DateTimeField()
-    
-    form_updated_by = models.ForeignKey(Account)
-    date_updated = models.DateTimeField()
-    
-    name = models.ForeignKey(BorderStation)
+
+class BorderStationBudgetCalculation(models.Model):
+    form_entered_by = models.ForeignKey(Account, related_name="form_entered_by_account")
+    form_updated_by = models.ForeignKey(Account, related_name="form_updated_by_account")
+
+    date_time_entered = models.DateTimeField(auto_now_add=True)
+    date_time_last_updated = models.DateTimeField(auto_now=True)
+
+    border_station = models.ForeignKey(BorderStation)
     
     communication_chair = models.BooleanField('Chair', default=False)
-    communication_manager =  models.BooleanField('Manager', default=False)
+    communication_manager = models.BooleanField('Manager', default=False)
     communication_number_of_staff_with_walkie_talkies = models.PositiveIntegerField('# of staff with walkie-talkies', null=True, blank=True)
     communication_each_staff = models.PositiveIntegerField('each staff', null=True, blank=True)
     
@@ -19,7 +21,8 @@ class BorderStationBudgetCalculationForm(models.Model):
     travel_manager_with_bike = models.BooleanField('Manager with Bike', default=False)
     travel_number_of_staff_using_bikes = models.PositiveIntegerField('# of staff using bikes', null=True, blank=True)
     travel_last_months_expense_for_sending_girls_home = models.FloatField()
-    travel_motorbike =  models.BooleanField('Motorbike', default=False)
+    travel_motorbike = models.BooleanField('Motorbike', default=False)
+    travel_motorbike_amount = models.PositiveIntegerField('Motorbike other amount', null=True, blank=True)
     
     administration_number_of_intercepts_last_month = models.PositiveIntegerField('# of intercepts last month', null=True, blank=True)
     administration_number_of_meetings_per_month = models.PositiveIntegerField('# of meetings per month', null=True, blank=True)
@@ -29,7 +32,7 @@ class BorderStationBudgetCalculationForm(models.Model):
     medical_last_months_expense = models.FloatField()
     
     miscellaneous_number_of_intercepts_last_month = models.PositiveIntegerField('# of intercepts last month', null=True, blank=True)
-    
+
     shelter_rent = models.FloatField()
     shelter_water = models.FloatField()
     shelter_electricity = models.FloatField()
@@ -43,7 +46,8 @@ class BorderStationBudgetCalculationForm(models.Model):
     awareness_contact_cards = models.BooleanField('Contact Cards', default=False)
     awareness_awareness_party = models.FloatField()
     awareness_sign_boards = models.FloatField()
-    
+
+
 class OtherBudgetItemCost(models.Model):
     name = models.CharField(max_length=255, blank=False)
     cost = models.FloatField()
--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,15 @@
+from braces.views import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView, View, DeleteView, CreateView, UpdateView
 from extra_views import CreateWithInlinesView, UpdateWithInlinesView, InlineFormSet
+from budget.models import InterceptionRecordListView
 
-# Create your views here.
 def budget_create(request):
-    return render(request,'budget/budget_calculation_form.html')
+    return render(request, 'budget/budget_calculation_form.html')
+
+
+class BudgetListView(
+        LoginRequiredMixin,
+        ListView):
+    model = BorderStationBudgetCalculationForm
+    paginate_by = 20
