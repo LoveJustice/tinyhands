2210ae15bf93faff094944c8037d5c7d191b18e0
Ben Duggan <benaduggan@gmail.com>
2014-11-15 19:30:52 -0500

    made the budget calc forms fully 'crudable'! still need some @rszempli up on this form styling biz.

--- budget/views.py
+++ budget/views.py
@@ -1,9 +1,9 @@
 from braces.views import LoginRequiredMixin
+from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 from extra_views import CreateWithInlinesView, UpdateWithInlinesView, InlineFormSet
-# from budget.models import InterceptionRecordListView
-from accounts.mixins import PermissionsRequiredMixin
+from rest_framework.exceptions import PermissionDenied
 from budget.forms import BorderStationBudgetCalculationForm
 from budget.models import BorderStationBudgetCalculation, OtherBudgetItemCost
 
@@ -16,9 +16,9 @@ class BudgetCalcCreateView(
         LoginRequiredMixin,
         CreateWithInlinesView):
     model = BorderStationBudgetCalculation
-    template_name = 'budget/budget_calculation_form.html'
+    template_name = 'budget/borderstationbudgetcalculation_form.html'
     form_class = BorderStationBudgetCalculationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('budget_list')
     inlines = [OtherBudgetItemCostFormInline]
 
 
@@ -26,3 +26,29 @@ class BudgetCalcListView(
         LoginRequiredMixin,
         ListView):
     model = BorderStationBudgetCalculation
+
+
+class BudgetCalcUpdateView(
+        LoginRequiredMixin,
+        UpdateWithInlinesView):
+    model = BorderStationBudgetCalculation
+    form_class = BorderStationBudgetCalculationForm
+    success_url = reverse_lazy('budget_list')
+    inlines = [OtherBudgetItemCostFormInline]
+
+
+class BudgetCalcDetailView(BudgetCalcUpdateView, LoginRequiredMixin):
+    def post(self, request, *args, **kwargs):
+        raise PermissionDenied
+
+
+class BudgetCalcDeleteView(DeleteView, LoginRequiredMixin):
+
+    model = BorderStationBudgetCalculation
+    success_url = reverse_lazy('budget_list')
+
+    def delete(self, request, *args, **kwargs):
+        if self.request.user.is_superuser:
+            return super(BudgetCalcDeleteView, self).delete(request)
+        else:
+            messages.error(request, "You have no power here!!!")
