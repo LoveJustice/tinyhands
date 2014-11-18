084ed0bc44df1f0a234bf675f483452717d41fa1
Justin Southworth <justin_southworth@taylor.edu>
2014-11-18 15:47:47 -0500

    Doin' work done on PDF generation.

--- budget/urls.py
+++ budget/urls.py
@@ -4,5 +4,6 @@ from budget import views
 urlpatterns = patterns('portal.views',
     url(r'^budget_calculations/create$', views.BudgetCalcCreateView.as_view(), name='budget_create'),
     url(r'^budget_calculations/', views.BudgetCalcListView.as_view(), name='budget_list'),
+    url(r'^getPDF/$', views.search_form),
 
 )
--- budget/views.py
+++ budget/views.py
@@ -26,3 +26,47 @@ class BudgetCalcListView(
         LoginRequiredMixin,
         ListView):
     model = BorderStationBudgetCalculation
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import Template, Context
+
+from z3c.rml import rml2pdf
+from reportlab import *
+import preppy
+import cStringIO
+
+
+def search_form(request):
+    return render_to_response('search_form.html')
+
+
+def getPDF(request):
+    """Returns PDF as a binary stream."""
+
+    if 'q' in request.GET:
+
+        rml = getRML(request.GET['q'])
+
+        buf = cStringIO.StringIO()
+
+        #create the pdf
+        rml2pdf.go(rml, outputFileName=buf)
+        buf.reset()
+        pdfData = buf.read()
+
+        #send the response
+        response = HttpResponse(mimetype='application/pdf')
+        response.write(pdfData)
+        response['Content-Disposition'] = 'attachment; filename=output.pdf'
+        return response
+
+def getRML(name):
+    """We used django template to write the RML, but you could use any other
+    template language of your choice.
+    """
+    t = Template(open('budget/templates/budget/test.rml').read())
+    c = Context({"name": name})
+    rml = t.render(c)
+    #django templates are unicode, and so need to be encoded to utf-8
+    return rml.encode('utf8')
