8433fe0e8c40ed3717dbf738072f3314e633bf45
dmoore <dmoore@cse20105.cs.local>
2014-11-20 16:16:11 -0500

    Initial accounts testing

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,5 @@
 from django_webtest import WebTest
-from django.utils import unittest
+from django.test import TestCase
 from accounts.models import Account, Alert
 
 
@@ -7,7 +7,7 @@ class TestModels(WebTest):
     pass
 
 
-class AlertTestCase(unittest.TestCase):
+class AlertTestCase(TestCase):
     def setUp(self):
         self.alert = Alert(1, 'code', 'template')
         self.alert.save()
@@ -20,4 +20,21 @@ class AlertTestCase(unittest.TestCase):
         x = Alert.objects.all()[0] #get the first alert in the database
         x.delete()
         self.assertEqual(len(Alert.objects.all()),0)
+
+class PermissionsTesting(TestCase):
+
+    fixtures = ['accounts.json']
+
+    def test_permissions_overview(self):
+        original = Account.objects.all()[0]
+        self.client.get(reverse('login'))
+
+        resp = self.client.get('/portal/dashboard/')
+        self.client.get(reverse('login'))
+        print(resp.status_code)
+        self.assertEqual(resp.status_code, 302)
+
+
+
+
 
