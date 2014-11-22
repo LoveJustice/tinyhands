db9725c1b96671a0e7d51557f7b7450ead9b961f
dmoore <dmoore@cse20105.cs.local>
2014-11-22 17:01:04 -0500

    Permissions testing for all three account groups.

--- accounts/tests/tests.py
+++ accounts/tests/tests.py
@@ -30,6 +30,7 @@ class PermissionsTesting(WebTest):
     def setUp(self):
         self.superuser = SuperUserFactory.create()
         self.viewuser = ViewUserFactory.create()
+        self.adduser = AddUserFactory.create()
         #print(Account.objects.all())
 
     def test_account_permissions_superuser(self):
@@ -44,7 +45,7 @@ class PermissionsTesting(WebTest):
         #Ensures a superuser can access page to delete an account.
         #account_delete = self.app.get(reverse('account_delete/0'), user=self.superuser)
         #self.assertEquals(account_delete.status_int, 200)
-        #print("This is the delete test")
+        #print("This is the superuser delete test")
 
         #Ensures a superuser can access the access controls page.
         access_control = self.app.get(reverse('access_control'), user=self.superuser)
@@ -61,7 +62,7 @@ class PermissionsTesting(WebTest):
         #Ensures a superuser can view the details of IRF's.
         #detail_IRF = self.app.get(reverse('interceptionrecord_detail'), user=self.superuser)
         #self.assertEquals(detail_IRF.status_int, 200)
-        #print("This is the detail IRF test")
+        #print("This is the superuser detail IRF test")
 
         #Ensures a superuser can create an IRF.
         create_IRF = self.app.get(reverse('interceptionrecord_create'), user=self.superuser)
@@ -84,14 +85,122 @@ class PermissionsTesting(WebTest):
         self.assertEquals(dashboard.status_int, 200)
 
         #Ensures a superuser can create a border station.
-        create_borderstation = self.app.get(reverse('borderstations_create'), user=self.superuser)
-        self.assertEquals(create_borderstation.status_int, 200)
-        #print("This is the create border stations test")
+        #create_borderstation = self.app.get(reverse('borderstations_create'), user=self.superuser)
+        #self.assertEquals(create_borderstation.status_int, 200)
 
     def test_account_permissions_viewuser(self):
         #Ensures a viewuser cannot access the account list.
         account_list = self.app.get(reverse('account_list'), user=self.viewuser, expect_errors=True)
         self.assertEquals(account_list.status_int, 403)
+
+        #Ensures a viewuser cannot access page to create a new account.
+        account_create = self.app.get(reverse('account_create'), user=self.viewuser, expect_errors=True)
+        self.assertEquals(account_create.status_int, 403)
+
+        #Ensures a viewuser cannot access page to delete an account.
+        #account_delete = self.app.get(reverse('account_delete/0'), user=self.viewuser, expect_errors=True)
+        #self.assertEquals(account_delete.status_int, 403)
+        #print("This is the viewuser cannot delete account test")
+
+        #Ensures a viewuser cannot access the access controls page.
+        access_control = self.app.get(reverse('access_control'), user=self.viewuser, expect_errors=True)
+        self.assertEquals(access_control.status_int, 403)
+
+        #Ensures a viewuser cannot access the access defaults page.
+        access_defaults = self.app.get(reverse('access_defaults'), user=self.viewuser, expect_errors=True)
+        self.assertEquals(access_defaults.status_int, 403)
+
+        #Ensures a viewuser can view the list of IRF's.
+        view_IRF = self.app.get(reverse('interceptionrecord_list'), user=self.viewuser)
+        self.assertEquals(view_IRF.status_int, 200)
+
+        #Ensures a viewuser can view the details of IRF's.
+        #detail_IRF = self.app.get(reverse('interceptionrecord_detail'), user=self.viewuser)
+        #self.assertEquals(detail_IRF.status_int, 200)
+        #print("This is the viewuser detail IRF test")
+
+        #Ensures a viewuser cannot create an IRF.
+        create_IRF = self.app.get(reverse('interceptionrecord_create'), user=self.viewuser, expect_errors=True)
+        self.assertEquals(create_IRF.status_int, 403)
+
+        #Ensures a viewuser can view the list of VIF's.
+        view_VIF = self.app.get(reverse('victiminterview_list'), user=self.viewuser)
+        self.assertEquals(view_VIF.status_int, 200)
+
+        #Ensures a viewuser cannot access the VDC administrator page.
+        vdc_admin = self.app.get(reverse('vdc_admin_page'), user=self.viewuser, expect_errors=True)
+        self.assertEquals(vdc_admin.status_int, 403)
+
+        #Ensures a viewuser cannot create a VDC.
+        vdc_create = self.app.get(reverse('vdc_create_page'), user=self.viewuser, expect_errors=True)
+        self.assertEquals(vdc_create.status_int, 403)
+
+        #Ensures a viewuser can view the main dashboard.
+        dashboard = self.app.get(reverse('main_dashboard'), user=self.viewuser)
+        self.assertEquals(dashboard.status_int, 200)
+
+        #Ensures a viewuser cannot create a border station.
+        create_borderstation = self.app.get(reverse('borderstations_create'), user=self.viewuser, expect_errors=True)
+        self.assertEquals(create_borderstation.status_int, 403)
+
+    def test_account_permissions_adduser(self):
+        #Ensures an adduser cannot access the account list.
+        account_list = self.app.get(reverse('account_list'), user=self.adduser, expect_errors=True)
+        self.assertEquals(account_list.status_int, 403)
+
+        #Ensures an adduser cannot access page to create a new account.
+        account_create = self.app.get(reverse('account_create'), user=self.adduser, expect_errors=True)
+        self.assertEquals(account_create.status_int, 403)
+
+        #Ensures an adduser cannot access page to delete an account.
+        #account_delete = self.app.get(reverse('account_delete/0'), user=self.adduser, expect_errors=True)
+        #self.assertEquals(account_delete.status_int, 403)
+        #print("This is the adduser cannot delete account test")
+
+        #Ensures an adduser cannot access the access controls page.
+        access_control = self.app.get(reverse('access_control'), user=self.adduser, expect_errors=True)
+        self.assertEquals(access_control.status_int, 403)
+
+        #Ensures an adduser cannot access the access defaults page.
+        access_defaults = self.app.get(reverse('access_defaults'), user=self.adduser, expect_errors=True)
+        self.assertEquals(access_defaults.status_int, 403)
+
+        #Ensures an adduser can view the list of IRF's.
+        view_IRF = self.app.get(reverse('interceptionrecord_list'), user=self.adduser)
+        self.assertEquals(view_IRF.status_int, 200)
+
+        #Ensures an adduser can view the details of IRF's.
+        #detail_IRF = self.app.get(reverse('interceptionrecord_detail'), user=self.adduser)
+        #self.assertEquals(detail_IRF.status_int, 200)
+        #print("This is the adduser detail IRF test")
+
+        #Ensures an adduser can create an IRF.
+        create_IRF = self.app.get(reverse('interceptionrecord_create'), user=self.adduser)
+        self.assertEquals(create_IRF.status_int, 200)
+
+        #Ensures an adduser can view the list of VIF's.
+        view_VIF = self.app.get(reverse('victiminterview_list'), user=self.adduser)
+        self.assertEquals(view_VIF.status_int, 200)
+
+        #Ensures an adduser cannot access the VDC administrator page.
+        vdc_admin = self.app.get(reverse('vdc_admin_page'), user=self.adduser, expect_errors=True)
+        self.assertEquals(vdc_admin.status_int, 403)
+
+        #Ensures an adduser can create a VDC.
+        vdc_create = self.app.get(reverse('vdc_create_page'), user=self.adduser)
+        self.assertEquals(vdc_create.status_int, 200)
+
+        #Ensures a viewuser can view the main dashboard.
+        dashboard = self.app.get(reverse('main_dashboard'), user=self.adduser)
+        self.assertEquals(dashboard.status_int, 200)
+
+        #Ensures a viewuser can create a border station.
+        #create_borderstation = self.app.get(reverse('borderstations_create'), user=self.adduser)
+        #self.assertEquals(create_borderstation.status_int, 200)
+
+    def test_account_actions(self):
+        pass
+        #Consider using the ideas written below by Colgan.
 
         # account_edit = account_list.click('Edit')
         # account_edit.form['email'] = 'dvcolgan@gmail.com'
