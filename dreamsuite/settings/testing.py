35c7a24cd6fc24585e649fdd1de8c1ae70badd4f
Justin Southworth <justin_southworth@taylor.edu>
2015-02-19 15:05:34 -0500

    Colgan's hacks to speed up tests.
    [skip ci]

--- dreamsuite/settings/testing.py
+++ dreamsuite/settings/testing.py
@@ -11,3 +11,19 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
     }
 }
+
+# Colgan's hacks to make tests run faster.
+# Use a faster (and unsecure) hash.
+ PASSWORD_HASHERS = (
+             'django.contrib.auth.hashers.MD5PasswordHasher',
+             )
+
+# Don't run migrations, just use the current models.
+ def prevent_tests_migrate(db):
+         import django
+             from django.db import connections
+                 from django.db.migrations.executor import MigrationExecutor
+                     django.setup()
+                         ma = MigrationExecutor(connections[db]).loader.migrated_apps
+                             return dict(zip(ma, ['{a}.notmigrations'.format(a=a) for a in ma]))
+                         MIGRATION_MODULES = prevent_tests_migrate('default')
