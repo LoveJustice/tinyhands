526588172c45c64549ef64cd0fddc2a3b5e16ed6
bsell <bryantsell@gmail.com>
2015-03-01 16:59:53 -0500

    Finish up borderstations update tests, and begin irf header tests

--- dataentry/tests/test_views.py
+++ dataentry/tests/test_views.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 
 from accounts.tests.factories import *
 
+from dataentry.models import BorderStation
 from accounts.models import Account
 
 class SearchFormsMixinTests(TestCase):
@@ -22,10 +23,11 @@ class InterceptionRecordListViewTests(WebTest):
 
 	def setUp(self):
 		self.superuser = SuperUserFactory.create()
+		self.response = self.app.get(reverse('interceptionrecord_list'), user=self.superuser)
+        self.form = self.response.form
 
 	def test_InterceptionRecordListView_exists(self):
-		response = self.app.get(reverse('interceptionrecord_list'), user=self.superuser)
-		self.assertEquals(response.status_code, 200)
+		self.assertEquals(self.response.status_code, 200)
 
 	def test_search_url_exists(self):
 		response = self.app.get('/data-entry/irfs/search/?search_value=BHD', user=self.superuser)
@@ -44,3 +46,23 @@ class VictimInterviewFormListViewTests(WebTest):
 	def test_search_url_exists(self):
 		response = self.app.get('/data-entry/vifs/search/?search_value=BHD', user=self.superuser)
 		self.assertEquals(response.status_code, 200)
+
+class InterceptionRecordCreateViewTests(WebTest):
+
+	def setUp(self):
+		self.superuser = SuperUserFactory.create()
+		self.response = self.app.get(reverse('interceptionrecord_create'), user=self.superuser)
+        self.form = self.response.form
+        
+    def test_irf_number_matches_existing_border_station(self):
+        form = self.form
+        
+        irfNumber = "BHD123"
+        
+        form.set("irf_number", irfNumber)
+        
+        self.assertTrue(False) # TODO: Validate IRF# contains code for existing borderstation
+        
+    # TODO: Validate IRF# is valid
+    # TODO: If IRF# isnt valid do something
+    # TODO: Dont let submit if IRF# isnt valid
