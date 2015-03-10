24d3ff07397268a2151575bc8666746376db9a6c
rszempli <rszemplinski22@gmail.com>
2015-03-10 15:42:11 -0400

    finishing up testing for vif

--- dataentry/tests/test_views.py
+++ dataentry/tests/test_views.py
@@ -47,7 +47,17 @@ class VictimInterviewFormListViewTests(WebTest):
 		response = self.app.get('/data-entry/vifs/search/?search_value=BHD', user=self.superuser)
 		self.assertEquals(response.status_code, 200)
 
+class InterceptionRecordFormViewTests(WebTest):
+    
+    fixture = ['geo-code-locations.json']
+    
+    def setUp(self):
+        self.superuser = SuperUserFactory.create();
+        
+    def test_user_can_create_irf_form(self):
+        pass
 
+    
 class VictimInterviewFormViewTests(WebTest):
 
     fixtures = ['geo-code-locations.json']
@@ -59,7 +69,7 @@ class VictimInterviewFormViewTests(WebTest):
         response = self.app.get(reverse('victiminterview_create'), user=self.superuser)
         self.assertEquals(response.status_code, 200)
 
-    def test_form_can_not_submit_without_required_fields_filled_out(self):
+    def test_form_submits_with_required_fields_and_redirects(self):
         response = self.app.get(reverse('victiminterview_create'), user=self.superuser)
         form = response.form
         form.set('vif_number', 'CND2')
@@ -90,3 +100,31 @@ class VictimInterviewFormViewTests(WebTest):
         form_2.set('ignore_warnings', True)
         form_response_2 = form_2.submit()
         self.assertEquals(form_response_2.status_code, 302)
+
+    def test_form_does_not_submit_without_required_fields(self):
+        response = self.app.get(reverse('victiminterview_create'), user=self.superuser)
+        form = response.form
+        form.set('vif_number', 'CND2')
+        form.set('date', datetime.datetime.now().strftime("%m/%d/%Y"))
+        form.set('interviewer', "johnny be good")
+        form.set('statement_read_before_beginning', True);
+        form.set('victim_gender', "male")
+        form.set('victim_name', "Sally Sue")
+        form.set('victim_address_district', "Dhanusa")
+        form.set('victim_address_vdc', 'Chalsa')
+        form.set('migration_plans_education', True)
+        form.set('primary_motivation_support_myself', True)
+        form.set('victim_primary_means_of_travel_local_bus', True)
+        form.set('victim_guardian_address_district', "Dhanusa")
+        form.set('victim_guardian_address_vdc', "Chalsa")
+        form.fields['victim_recruited_in_village'][0].checked = True
+        form.fields['victim_stayed_somewhere_between'][0].checked = True
+        form.set('meeting_at_border_yes', True)
+        form.fields['victim_knew_details_about_destination'][0].checked = True
+        form.set('awareness_before_interception_had_heard_not_how_bad', True)
+        form.set('legal_action_against_traffickers_no', True)
+        form.set('attitude_towards_tiny_hands_thankful', True)
+        form.set('victim_heard_gospel_no', True)
+        form_response = form.submit()
+        field_errors = form_response.context['form'].errors
+        self.assertEquals(field_errors['has_signature'][0], "This field is required.")
