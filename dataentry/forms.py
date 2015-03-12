b06768efe2cf0543e2a46a11ea1924119047cc94
bsell <bryantsell@gmail.com>
2015-03-12 15:14:16 -0400

    Added vif validation and corresponding tests to make sure it works.

--- dataentry/forms.py
+++ dataentry/forms.py
@@ -653,6 +653,8 @@ class VictimInterviewForm(DreamSuitePaperForm):
     def clean(self):
         cleaned_data = super(VictimInterviewForm, self).clean()
         self.has_warnings = False
+        
+        self.ensure_valid_vif_number(cleaned_data)
 
         for field_name_start in [
             'primary_motivation',
@@ -672,6 +674,16 @@ class VictimInterviewForm(DreamSuitePaperForm):
             self.ensure_tiny_hands_rating(cleaned_data)
             
         return cleaned_data
+            
+    def ensure_valid_vif_number(self, cleaned_data):
+        BSCode = cleaned_data['vif_number'][:3]
+        formNumberLength = len(cleaned_data['vif_number'][3:])
+        codeLength = len(BSCode)
+        borderstations = BorderStation.objects.all().filter(station_code=BSCode)
+        if( formNumberLength == 0 ):
+            self._errors['vif_number'] = self.error_class(['Invalid VIF Number. Please add a number after the Border Station code.'])
+        if( len(borderstations) == 0 or codeLength != 3):
+            self._errors['vif_number'] = self.error_class(['Invalid VIF Number. Create one that includes an existing Border Station code.'])
 
     def ensure_victim_where_going(self, cleaned_data):
         if not self.at_least_one_checked(cleaned_data, 'victim_where_going'):
