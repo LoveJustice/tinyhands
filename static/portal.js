33a10bac65a11451ece80d146f36de4eb7d58ef0
Ben Duggan <benaduggan@gmail.com>
2015-03-22 22:24:02 -0400

    Refactoring

--- static/portal.js
+++ static/portal.js
@@ -1,4 +1,5 @@
 function initialize() {
+    //initialize the map
     var mapOptions = {
       center: { lat: 28.394857, lng: 84.124008},
       zoom: 8,
@@ -18,8 +19,6 @@ function initialize() {
             styleId: 2,
             templateId: 2
         }});
-
-
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     var homeControlDiv = document.createElement('div');
@@ -75,34 +74,50 @@ function toggleVDCOverlay(layer, map) {
     }
 }
 
-function resizeMap() {
+function resizeMap() { // keeping the controls visibile on the page at all times
     var map_canvas = document.getElementById('map-canvas');
     map_canvas.style.width = window.innerWidth+"px";
     map_canvas.style.height = (window.innerHeight-50)+"px";
 }
 
-function getContentString(borderStation){
+function getStaticContentString(borderStation) { //This is what the content of a static border_station is
     return '<div id="StationWindow" class="dashboardInfoWindow">' +
-	    '<h3>'+borderStation.fields.station_name+'</h3>' +
-	    '<p>Est. '+borderStation.fields.date_established+'</p>' +
-	    '<p>'+borderStation.fields.station_code+'</p>' +
-        '<p>Shelter: '+borderStation.fields.has_shelter+'</p>' +
+        '<h3>' + borderStation.fields.station_name + ' - ' + borderStation.fields.station_code + '</h3>' +
+
+        '<p>Est. ' + borderStation.fields.date_established + '</p>' +
+        '<p>Has shelter: ' + hasShelter(borderStation) + '</p>' +
         '<p id="stationInterception">Interceptions: ' + '</p>' +
         '<p id="staffset"># of Staff ' + '</p>' +
-	    '</div>';
+        '</div>';
+}
+
+function getDyanamicContentString(borderStation){ //This is what the content of a static border_station is
+    return '<div id="StationWindow" class="dashboardInfoWindow">' +
+        '<h3>' + borderStation.fields.station_name + ' - ' + borderStation.fields.station_code + '</h3>' +
+
+        '<p>Est. ' + borderStation.fields.date_established + '</p>' +
+        '<p>Has shelter: ' + hasShelter(borderStation) + '</p>' +
+        '<p id="stationInterception">Interceptions: ' + '</p>' +
+        '<p id="staffset"># of Staff ' + '</p>' +
+        '</div>';
 }
 
 function getBorderStations(map){
-    $.get("/portal/get_border_stations",function(data,status){
-        var infowindow = new google.maps.InfoWindow({maxWidth: 1000});
-        var dynamicWindow = new google.maps.InfoWindow({maxWidth: 1000});
-        for(var station=0;station<data.length;station++){
+    /*  Runs on page load to show all the border stations on the
+     *  map and set all of the onClick/onHover events for them
+     */
+    $.get("/portal/get_border_stations",function(data, status){
+        var infowindow = new google.maps.InfoWindow(); //Initialize the Static Border Station view
+        var dynamicWindow = new google.maps.InfoWindow(); //Initialize the Dynamic Border Station view
+
+        for(var station=0;station<data.length;station++){ //Iterate over each Border Station
             var myLatlng = new google.maps.LatLng(data[station].fields.latitude,data[station].fields.longitude);
-            var marker = new google.maps.Marker({
+            var marker = new google.maps.Marker({ //Initialize a BorderStation's marker
                 position: myLatlng,
                 map: map,
                 title: data[station].fields.station_name + " " + data[station].fields.station_code
             });
+
             google.maps.event.addListener(marker, 'mouseout', (function(marker, station) {
                 return function() {
                     infowindow.close();
@@ -111,10 +126,10 @@ function getBorderStations(map){
                     });
                 }
             })(marker, station));
-            google.maps.event.addListener(marker, 'mouseover', (function(marker, station) {
+
+            google.maps.event.addListener(marker, 'mouseover', (function(marker, station) { //For the Static View
                 return function() {
-                    infowindow.setContent(getContentString(data[station]));
-                    console.log(data[station].fields.station_name);
+                    infowindow.setContent(getStaticContentString(data[station]));
 
                     //gets the number of irfs
                     $.get("/portal/get_interception_records", {station_code: data[station].fields.station_code}, function(data){
@@ -128,30 +143,36 @@ function getBorderStations(map){
 
                     infowindow.open(map, this);
 
-                    $(".gm-style-iw").each(function() {
+                    $(".gm-style-iw").each(function() { // TODO: We are resizing according to the length of the station name? we need a better solution for this!
                         if(data[station].fields.station_name.length > 10) {
                             $(this).addClass('station-info-window-big');
-                            console.log(data[station].station_code);
                         }
                         $(this).addClass('station-info-window');
                     });
                 }
             })(marker, station));
 
-            google.maps.event.addListener(marker, 'click', (function(marker, station) {
+            google.maps.event.addListener(marker, 'click', (function(marker, station) { //For the Dynamic view
                 return function() {
                     infowindow.close();
-                    dynamicWindow.setContent(getContentString(data[station]));
-                    console.log(data[station].fields.station_name);
+
+                    dynamicWindow.setContent(getDyanamicContentString(data[station]));
+
+                    //gets the number of IRFs to date
                     $.get("/portal/get_interception_records", {station_code: data[station].fields.station_code}, function(data){
                         $("#stationInterception").text("Interceptions: " + data);
                     });
 
+                    //gets the number of staff
+                    $.get("/portal/get_staff_count", {station_code: data[station].fields.station_code}, function(data){
+                        $("#staffset").text('# of Staff:' + data);
+                    });
+
                     dynamicWindow.open(map, this);
+
                     $(".gm-style-iw").each(function() {
                         if(data[station].fields.station_name.length > 10) {
                             $(this).addClass('station-info-window-big');
-                            console.log(data[station].station_code);
                         }
                         $(this).addClass('station-info-window');
                     });
@@ -159,6 +180,15 @@ function getBorderStations(map){
             })(marker, station));
         }
     });
+}
+
+function hasShelter(borderStation){ // basically just converts a true/false to a yes/no
+    if(borderStation.fields.has_shelter) {
+        return "Yes";
+    }
+    else {
+        return "No";
+    }
 }
 
 $( window ).resize(function() {
@@ -169,4 +199,3 @@ $(function() {
     google.maps.event.addDomListener(window, 'load', initialize);
     resizeMap();
 });
-
