492a8dd594bdeb253c091412ec15d719367b301d
Ben Duggan <benaduggan@gmail.com>
2015-04-28 11:20:00 -0400

    unit testing and trying to figure out why my angular stuff all of a sudden completely stopped working

--- budget/tests.py
+++ budget/tests.py
@@ -1,18 +1,44 @@
-from datetime import timedelta
-
-from django.core.urlresolvers import reverse
-from django.utils import timezone
 from django_webtest import WebTest
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
 
-from budget.factories import *
+from static_border_stations.tests.factories import BorderStationFactory
+
 import ipdb
 
+
 class BudgetCalcApiTests(WebTest):
-
     def setUp(self):
-        #timezone.now() gets utc time but timezone.now().now() gets local Nepal time
-        budgetCalc = BorderStationBudgetCalculationFactory._create()
-        other_budget_item = OtherBudgetItemCostFactory._create()
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.border_station = BorderStationFactory()
 
-    def test(self):
+    def testCreateBudgetSheet(self):
+        response = self.client.post('/budget/api/budget_calculations/', {"border_station": self.border_station.pk})
+        self.assertEqual(response.status_code, 201)
+
+    def testRemoveBudgetSheet(self):
         ipdb.set_trace()
+
+
+        response = self.client.post('/budget/api/budget_calculations/', {"border_station": self.border_station.pk})
+        budget_id = response.data.get('id')
+        self.assertEqual(response.status_code, 201)
+
+        # delete the form
+        response = self.client.delete('/budget/api/budget_calculations/' + str(budget_id))
+        # self.assertEqual(response.status_code, 201)
+
+        # count the remaining forms
+        response = self.client.get('/budget/api/budget_calculations/')
+        #self.assertEqual(response.status_code, 201)
+
+
+
+
+
+
+
+
+
+
