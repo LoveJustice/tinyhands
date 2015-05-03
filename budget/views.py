14d2a0d167b71cb5f9b9525440b1f2637d25740b
Ben Duggan <benaduggan@gmail.com>
2015-05-03 18:08:25 -0400

    Aesthetics
    - Moved the budget calculatins button to the top bar
    - Added a message if there are no budget worksheets in the system

--- budget/views.py
+++ budget/views.py
@@ -119,117 +119,18 @@ class StaffSalaryViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
 
 
-@login_required
-def budget_calc_create(request, pk):
-    #is there a better way to do permissions in function based views?
-    if not request.user.permission_budget_manage:
-        return redirect("home")
-
-    border_station = get_object_or_404(BorderStation, pk=pk)
-    border_station_staff = border_station.staff_set.all()
-    form = BorderStationBudgetCalculationForm()
-
-    StaffFormSet = modelformset_factory(model=Staff, extra=0, fields=['salary'])
-
-    OtherItemsFormset = inlineformset_factory(BorderStationBudgetCalculation, OtherBudgetItemCost, extra=1, fields=['name', 'cost'])
-
-    if request.method == "POST":
-        staff_formset = StaffFormSet(request.POST, queryset=border_station_staff, prefix='staff')
-        form = BorderStationBudgetCalculationForm(request.POST)
-
-        travel_items_formset = OtherItemsFormset(request.POST, prefix='travel')
-        misc_items_formset = OtherItemsFormset(request.POST, prefix='misc')
-        awareness_items_formset = OtherItemsFormset(request.POST, prefix='awareness')
-        supplies_items_formset = OtherItemsFormset(request.POST, prefix='supplies')
-
-        if form.is_valid() and staff_formset.is_valid() and travel_items_formset.is_valid() and misc_items_formset.is_valid() and awareness_items_formset.is_valid() and supplies_items_formset.is_valid():
-            form.instance.border_station = border_station
-            form.save()
-            staff_formset.save()
-
-            save_all(travel_items_formset, 1, form)
-            save_all(misc_items_formset, 2, form)
-            save_all(awareness_items_formset, 3, form)
-            save_all(supplies_items_formset, 4, form)
-
-            return redirect("budget_list")
-
-    else:
-        staff_formset = StaffFormSet(queryset=border_station_staff, prefix='staff')
-
-        travel_items_formset = OtherItemsFormset(prefix='travel')
-        misc_items_formset = OtherItemsFormset(prefix='misc')
-        awareness_items_formset = OtherItemsFormset(prefix='awareness')
-        supplies_items_formset = OtherItemsFormset(prefix='supplies')
-
-    submit_type = "Create"
-    return render(request, 'budget/borderstationbudgetcalculation_form.html', locals())
-
-
-def save_all(other_formset, form_section, budget_calc_form):
-    for form in other_formset:
-        form.instance.budget_item_parent = budget_calc_form.instance
-        form.instance.form_section = form_section
-        if form.instance.cost > 0 or form.instance.name != '':
-            form.save()
-        if form.cleaned_data:
-            if form.cleaned_data["DELETE"]:
-                form.instance.delete()
-    return True
-
-
-@login_required
-def budget_calc_update(request, pk):
-    #is there a better way to do permissions in function based views?
-    if not request.user.permission_budget_manage:
-        return redirect("home")
-
-    budget_calc = get_object_or_404(BorderStationBudgetCalculation, pk=pk)
-    form = BorderStationBudgetCalculationForm(instance=budget_calc)
-
-    border_station = budget_calc.border_station
-    border_station_staff = border_station.staff_set.all()
-    StaffFormSet = modelformset_factory(model=Staff, extra=0, fields=['salary'])
-
-    OtherItemsFormset = inlineformset_factory(BorderStationBudgetCalculation, OtherBudgetItemCost, extra=1, fields=['name', 'cost'])
-
-    if request.method == "POST":
-        staff_formset = StaffFormSet(request.POST, queryset=border_station_staff, prefix='staff')
-        form = BorderStationBudgetCalculationForm(request.POST, instance=budget_calc, empty_permitted=True)
-
-        travel_items_formset = OtherItemsFormset(request.POST, instance=budget_calc, queryset=budget_calc.otherbudgetitemcost_set.filter(form_section=1), prefix='travel')
-        misc_items_formset = OtherItemsFormset(request.POST, instance=budget_calc, queryset=budget_calc.otherbudgetitemcost_set.filter(form_section=2), prefix='misc')
-        awareness_items_formset = OtherItemsFormset(request.POST, instance=budget_calc, queryset=budget_calc.otherbudgetitemcost_set.filter(form_section=3), prefix='awareness')
-        supplies_items_formset = OtherItemsFormset(request.POST, instance=budget_calc, queryset=budget_calc.otherbudgetitemcost_set.filter(form_section=4), prefix='supplies')
-
-        if form.is_valid() and staff_formset.is_valid() and travel_items_formset.is_valid() and misc_items_formset.is_valid() and awareness_items_formset.is_valid() and supplies_items_formset.is_valid():
-            form.save()
-            staff_formset.save()
-
-            save_all(travel_items_formset, 1, form)
-            save_all(misc_items_formset, 2, form)
-            save_all(awareness_items_formset, 3, form)
-            save_all(supplies_items_formset, 4, form)
-
-            return redirect("budget_list")
-    else:
-        staff_formset = StaffFormSet(queryset=border_station_staff, prefix='staff')
-
-        travel_items_formset = OtherItemsFormset(instance=budget_calc, queryset=budget_calc.otherbudgetitemcost_set.filter(form_section=1), prefix='travel')
-        misc_items_formset = OtherItemsFormset(instance=budget_calc, queryset=budget_calc.otherbudgetitemcost_set.filter(form_section=2), prefix='misc')
-        awareness_items_formset = OtherItemsFormset(instance=budget_calc, queryset=budget_calc.otherbudgetitemcost_set.filter(form_section=3), prefix='awareness')
-        supplies_items_formset = OtherItemsFormset(instance=budget_calc, queryset=budget_calc.otherbudgetitemcost_set.filter(form_section=4), prefix='supplies')
-
-    submit_type = "Update"
-    return render(request, 'budget/borderstationbudgetcalculation_form.html', locals())
-
-
 class BudgetCalcListView(
         LoginRequiredMixin,
         ListView):
     model = BorderStationBudgetCalculation
     border_stations = BorderStation.objects.all()
     permissions_required = ['permission_budget_manage']
+
+    def get_context_data(self, **kwargs):
+        context = super(BudgetCalcListView, self).get_context_data(**kwargs)
+        if BorderStationBudgetCalculation.objects.all().count()==0:
+            context["none_in_system"] = True
+        return context
 
 
 class PDFView(View):
