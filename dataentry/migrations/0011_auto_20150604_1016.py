a16364e2bb783de80f8986107b05c99a5fc2cde5
amun33 <amunn33@gmail,com>
2015-06-04 10:50:48 +0545

    Migration file included

---
+++ dataentry/migrations/0011_auto_20150604_1016.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dataentry', '0010_auto_20150214_0223'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='interceptee',
+            name='district',
+            field=models.ForeignKey(blank=True, to='dataentry.District', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='interceptee',
+            name='vdc',
+            field=models.ForeignKey(blank=True, to='dataentry.VDC', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='victiminterview',
+            name='victim_address_district',
+            field=models.ForeignKey(related_name='victim_address_district', to='dataentry.District', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='victiminterview',
+            name='victim_address_vdc',
+            field=models.ForeignKey(related_name='victim_address_vdc', to='dataentry.VDC', null=True),
+            preserve_default=True,
+        ),
+    ]
