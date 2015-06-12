29b7b67df91e86d0512cae4dc6413bea48cf853a
amun33 <amunn33@gmail,com>
2015-06-12 12:05:30 +0545

    Initial testing and working on required fields

--- static_border_stations/forms.py
+++ static_border_stations/forms.py
@@ -6,15 +6,20 @@ from static_border_stations.models import Staff, CommitteeMember, Location
 class StaffForm(forms.ModelForm):
     class Meta:
         model = Staff
-
+        exclude = []
+    def __init__(self, *args, **kwargs):
+        super(StaffForm, self).__init__(*args, **kwargs)
+        self.fields['email'].required = False
 
 class CommitteeMemberForm(forms.ModelForm):
+    email = forms.EmailField(max_length=255, unique=True, required=False)
     class Meta:
         model = CommitteeMember
-        exclude = []
+        exclude = ['email']
 
 
 class LocationForm(forms.ModelForm):
+    email = forms.EmailField(max_length=255, unique=True, required=False)
     class Meta:
         model = Location
-        exclude = []
+        exclude = ['email']
