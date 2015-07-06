5fff415768a2af185b81a3885c0d1487497f1249
Justin Southworth <justin_southworth@taylor.edu>
2015-07-06 13:12:20 -0400

    Enhancing Austin's datatables fixes
     - Added Bootstrap
     - Refactored all the things to increase DRYness
     - Prettifying.

--- budget/static/budget/js/budget-datatable.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function() {
-  $('#budget_table').dataTable( {
-    "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 5, 6, 7, 8], } ],
-    "bFilter": true,
-    "bInfo": false,
-    "bPaginate": true,
-  });
-});
