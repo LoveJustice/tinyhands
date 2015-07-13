258fa89f7283d50531d04c8bebd0c659e983064d
amun33 <amunn33@gmail,com>
2015-07-13 14:13:24 +0545

    Test to ensure user cannot edit while viewing IRF

--- e2etesting/dataentry/irfCRUD.spec.js
+++ e2etesting/dataentry/irfCRUD.spec.js
@@ -74,6 +74,12 @@ describe('Interception Record Form -', function() {
             expect(element(by.id('id_has_signature')).isSelected()).toBeTruthy();
             browser.get(c.webAddress + '/data-entry/irfs/search/');
         });
+
+        it('Cannot Edit while Viewing IRF', function() {
+            irfPage.viewIRF();
+            browser.sleep(500);
+            expect(element(by.id('id_irf_number')).getAttribute('disabled')).toEqual('true');
+        });
     });
 });
 
