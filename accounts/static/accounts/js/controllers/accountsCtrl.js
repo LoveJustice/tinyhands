a684e85b75431678f23c1b640252753484d909c3
Jacob Jin <yage_jin@taylor.edu>
2015-10-22 14:08:14 -0400

    fixed some little bug so that angular works

--- accounts/static/accounts/js/controllers/accountsCtrl.js
+++ accounts/static/accounts/js/controllers/accountsCtrl.js
@@ -15,7 +15,25 @@ angular
                          "user_designation": "Super Administrator",
                          "date_created": "10.15.2015 15:37PM",
                          "last_login": "10.15.2015 15:37PM"}];
-        vm.foo = "testing"
+        vm.permissions = [{"full_name": "test1",
+                          "user_designation": "Super Administrator",
+                          "irf_view": true,
+                          "irf_add": true,
+                          "irf_edit": true,
+                          "irf_delete": true,
+                          "vif_view": true,
+                          "vif_add": true,
+                          "vif_edit": true,
+                          "vif_delete": true,
+                          "order_stations_view": true,
+                          "order_stations_add": true,
+                          "order_stations_edit": true,
+                          "order_stations_delete": true,
+                          "accounts_manage": true,
+                          "receive_mail": true,
+                          "vdc_message": true,
+                          "budget_manage": true}];
+        vm.foo = "testing";
 
 
     }]);
