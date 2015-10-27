7bfd83d410dbf602cb136429cb44ca4e6f68eb39
Andrew Smith <andrew@jrbeutler.com>
2015-10-27 15:53:32 -0400

    Added is_used_by_accounts field to DefaultPermissionsSetSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,6 +7,9 @@ class AccountsSerializer(serializers.ModelSerializer):
         exclude = ['password', 'activation_key', 'groups','user_permissions', 'is_staff', 'is_superuser']
 
 class DefaultPermissionsSetSerializer(serializers.ModelSerializer):
+    is_used_by_accounts = serializers.BooleanField();
+    
     class Meta:
         model = DefaultPermissionsSet
         exclude = []
+        read_only_fields = ['is_used_by_accounts']
