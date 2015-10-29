1f7b2fcfe993626c785d0be274d5dc70a3a7d948
bsell <bryantsell@gmail.com>
2015-10-29 17:02:54 -0400

    Start on e2e tests

--- e2etesting/borderStations/borderStationCRUD.js
+++ e2etesting/borderStations/borderStationCRUD.js
@@ -16,49 +16,49 @@ var borderStationPage = function() {
     };
 
     this.fillOutBorderStation = function() {
-        this.station_name = element(by.id("id_station_name")).sendKeys(c.stationName);
-        this.station_code = element(by.id("id_station_code")).sendKeys(c.stationCode);
-        this.date_established = element(by.id("id_date_established")).sendKeys(c.dateEstablished);
-        this.has_shelter = element(by.id("id_has_shelter")).click();
-        this.latitude = element(by.id("id_latitude")).sendKeys(c.latitude);
-        this.longitude = element(by.id("id_longitude")).sendKeys(c.longitude);
+        this.station_name = element(by.id("stationName")).sendKeys(c.stationName);
+        this.station_code = element(by.id("stationCode")).sendKeys(c.stationCode);
+        this.date_established = element(by.id("dateEstablished")).sendKeys(c.dateEstablished);
+        this.has_shelter = element(by.id("hasShelter")).click();
+        this.latitude = element(by.id("latitude")).sendKeys(c.latitude);
+        this.longitude = element(by.id("longitude")).sendKeys(c.longitude);
 
-        this.staff0_first_name = element(by.id("id_staff_set-0-first_name")).sendKeys(c.staff0SetFirstName);
-        this.staff0_last_name = element(by.id("id_staff_set-0-last_name")).sendKeys(c.staff0SetLastName);
-        this.staff0_email = element(by.id("id_staff_set-0-email")).sendKeys(c.staff0Email);
-        this.staff0_receives_money_distribution_form = element(by.id("id_staff_set-0-receives_money_distribution_form")).click();
+        this.staff0_first_name = element(by.id("S-firstName-0")).sendKeys(c.staff0SetFirstName);
+        this.staff0_last_name = element(by.id("S-lastName-0")).sendKeys(c.staff0SetLastName);
+        this.staff0_email = element(by.id("S-email-0")).sendKeys(c.staff0Email);
+        this.staff0_receives_money_distribution_form = element(by.id("S-receivesMoneyDistributionForm-0")).click();
 
-        this.committee0_first_name = element(by.id("id_committeemember_set-0-first_name")).sendKeys(c.committee0SetFirstName);
-        this.committee0_last_name = element(by.id("id_committeemember_set-0-last_name")).sendKeys(c.committee0SetLastName);
-        this.committee0_email = element(by.id("id_committeemember_set-0-email")).sendKeys(c.committee0Email);
-        this.committee0_receives_money_distribution_form = element(by.id("id_committeemember_set-0-receives_money_distribution_form")).click();
+        this.committee0_first_name = element(by.id("C-firstName-0")).sendKeys(c.committee0SetFirstName);
+        this.committee0_last_name = element(by.id("C-lastName-0")).sendKeys(c.committee0SetLastName);
+        this.committee0_email = element(by.id("C-email-0")).sendKeys(c.committee0Email);
+        this.committee0_receives_money_distribution_form = element(by.id("C-receivesMoneyDistributionForm-0")).click();
 
-        this.location0_set_name = element(by.id("id_location_set-0-name")).sendKeys(c.location0SetName);
-        this.location0_set_latitude = element(by.id("id_location_set-0-latitude")).sendKeys(c.location0SetLatitude);
+        this.location0_set_name = element(by.id("locationName-0")).sendKeys(c.location0SetName);
+        this.location0_set_latitude = element(by.id("latitude-0")).sendKeys(c.location0SetLatitude);
         browser.sleep(500);
-        this.location0_set_longitude = element(by.id("id_location_set-0-longitude")).sendKeys(c.location0SetLongitude).submit();
+        this.location0_set_longitude = element(by.id("longitude-0")).sendKeys(c.location0SetLongitude).submit();
         browser.sleep(500);
     };
 
     this.fillOutBorderStationWithNoEmail = function() {
-        this.station_name = element(by.id("id_station_name")).sendKeys(c.stationName);
-        this.station_code = element(by.id("id_station_code")).sendKeys(c.stationCode);
-        this.date_established = element(by.id("id_date_established")).sendKeys(c.dateEstablished);
-        this.has_shelter = element(by.id("id_has_shelter")).click();
-        this.latitude = element(by.id("id_latitude")).sendKeys(c.latitude);
-        this.longitude = element(by.id("id_longitude")).sendKeys(c.longitude);
+        this.station_name = element(by.id("stationName")).sendKeys(c.stationName);
+        this.station_code = element(by.id("stationCode")).sendKeys(c.stationCode);
+        this.date_established = element(by.id("dateEstablished")).sendKeys(c.dateEstablished);
+        this.has_shelter = element(by.id("hasShelter")).click();
+        this.latitude = element(by.id("latitude")).sendKeys(c.latitude);
+        this.longitude = element(by.id("longitude")).sendKeys(c.longitude);
 
-        this.staff0_first_name = element(by.id("id_staff_set-0-first_name")).sendKeys(c.staff0SetFirstName);
-        this.staff0_last_name = element(by.id("id_staff_set-0-last_name")).sendKeys(c.staff0SetLastName);
-        this.staff0_receives_money_distribution_form = element(by.id("id_staff_set-0-receives_money_distribution_form")).click();
+        this.staff0_first_name = element(by.id("S-firstName-0")).sendKeys(c.staff0SetFirstName);
+        this.staff0_last_name = element(by.id("S-lastName-0")).sendKeys(c.staff0SetLastName);
+        this.staff0_receives_money_distribution_form = element(by.id("S-receivesMoneyDistributionForm-0")).click();
 
-        this.committee0_first_name = element(by.id("id_committeemember_set-0-first_name")).sendKeys(c.committee0SetFirstName);
-        this.committee0_last_name = element(by.id("id_committeemember_set-0-last_name")).sendKeys(c.committee0SetLastName);
-        this.committee0_receives_money_distribution_form = element(by.id("id_committeemember_set-0-receives_money_distribution_form")).click();
+        this.committee0_first_name = element(by.id("C-firstName-0")).sendKeys(c.committee0SetFirstName);
+        this.committee0_last_name = element(by.id("C-lastName-0")).sendKeys(c.committee0SetLastName);
+        this.committee0_receives_money_distribution_form = element(by.id("C-receivesMoneyDistributionForm-0")).click();
 
-        this.location0_set_name = element(by.id("id_location_set-0-name")).sendKeys(c.location0SetName);
-        this.location0_set_latitude = element(by.id("id_location_set-0-latitude")).sendKeys(c.location0SetLatitude);
-        this.location0_set_longitude = element(by.id("id_location_set-0-longitude")).sendKeys(c.location0SetLongitude).submit();
+        this.location0_set_name = element(by.id("locationName-0")).sendKeys(c.location0SetName);
+        this.location0_set_latitude = element(by.id("latitude-0")).sendKeys(c.location0SetLatitude);
+        this.location0_set_longitude = element(by.id("longitude-0")).sendKeys(c.location0SetLongitude).submit();
 
     }
 
