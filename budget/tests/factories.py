2d97a74eac561d9db16c9a922519b13c846636d5
Ben Duggan <benaduggan@gmail.com>
2015-10-30 16:08:00 -0400

    trying to cut back the warnings

--- budget/tests/factories.py
+++ budget/tests/factories.py
@@ -1,7 +1,7 @@
 import datetime
 import factory
-from factory.django import DjangoModelFactory
 import pytz
+from factory.django import DjangoModelFactory
 from budget.models import BorderStationBudgetCalculation
 from static_border_stations.tests.factories import BorderStationFactory
 
@@ -11,4 +11,4 @@ class BorderStationBudgetCalculationFactory(DjangoModelFactory):
         model = BorderStationBudgetCalculation
 
     border_station = factory.SubFactory(BorderStationFactory)
-    month_year = datetime.datetime(2015, 5, 5, 5, 5, 5, tzinfo=pytz.UTC)
+    month_year = datetime.datetime(2015, 5, 5, 5, tzinfo=pytz.UTC)
