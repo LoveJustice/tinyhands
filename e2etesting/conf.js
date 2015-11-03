82196db5ae00fb2042e64352d979f75c43b859f5
Ben Duggan <benaduggan@gmail.com>
2015-11-03 13:24:39 -0500

    all tests passing

--- e2etesting/conf.js
+++ e2etesting/conf.js
@@ -18,8 +18,9 @@ exports.config = {
         'dataentry/irfCRUD.spec.js',
         'dataentry/vifCrud.spec.js',
         'dataentry/search.spec.js', //dependent on vifCrud.spec.js and irfCRUD.spec.js
+        //
 
-         //These three are dependent on each other
+        //These three are dependent on each other
         'borderStations/borderStationCRUD.spec.js',
         'budget/budgetForm.spec.js',
         'budget/moneyDistributionForm.spec.js',
@@ -29,8 +30,8 @@ exports.config = {
         'addresses/vdcAdminPage.spec.js',
         'addresses/address1Page.spec.js',
         'accounts/accessDefaults.spec.js',
-        // Has to run last
-        'accounts/permissionsPage.spec.js'
+
+        'accounts/permissionsPage.spec.js' // Has to run last
       ],
 
     framework: 'jasmine2',
