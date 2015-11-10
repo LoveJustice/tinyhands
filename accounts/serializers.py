6be20de4a435eee7cb247f7a459d9f83a3a7e3d6
Andrew Smith <andrew@jrbeutler.com>
2015-11-10 15:03:30 -0500

    Added has_been_activated field to Account serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from accounts.models import Account, DefaultPermissionsSet
 
 class AccountsSerializer(serializers.ModelSerializer):
+    has_been_activated = serializers.BooleanField(source='has_usable_password')
+    
     class Meta:
         model = Account
         exclude = ['password', 'activation_key', 'groups','user_permissions', 'is_staff', 'is_superuser']
+        read_only_fields = ['has_been_activated']
 
 class DefaultPermissionsSetSerializer(serializers.ModelSerializer):
     is_used_by_accounts = serializers.BooleanField();
