f7e36c27f4c50ccde663738c43a577bb8ec28687
Ben Duggan <benaduggan@gmail.com>
2015-11-09 22:11:31 -0500

    initial setup

--- dreamsuite/settings/local.py
+++ dreamsuite/settings/local.py
@@ -4,3 +4,12 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 SITE_DOMAIN = 'localhost:8080'
+
+INSTALLED_APPS += ('corsheaders',)
+
+MIDDLEWARE_CLASSES += (
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
