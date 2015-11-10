080d38edfd46afe0021a563440b4021b49e45fe8
Andrew Smith <andrew@jrbeutler.com>
2015-11-10 15:19:02 -0500

    Fixed api errors due to misconfigured Account serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,17 +2,15 @@ from rest_framework import serializers
 from accounts.models import Account, DefaultPermissionsSet
 
 class AccountsSerializer(serializers.ModelSerializer):
-    has_been_activated = serializers.BooleanField(source='has_usable_password')
+    has_been_activated = serializers.BooleanField(source='has_usable_password', read_only=True)
     
     class Meta:
         model = Account
         exclude = ['password', 'activation_key', 'groups','user_permissions', 'is_staff', 'is_superuser']
-        read_only_fields = ['has_been_activated']
 
 class DefaultPermissionsSetSerializer(serializers.ModelSerializer):
-    is_used_by_accounts = serializers.BooleanField();
+    is_used_by_accounts = serializers.BooleanField(read_only=True);
     
     class Meta:
         model = DefaultPermissionsSet
         exclude = []
-        read_only_fields = ['is_used_by_accounts']
