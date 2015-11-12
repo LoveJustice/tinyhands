0f67a55ba05451f325caa626db165acd80af6bb0
Jacob Jin <yage_jin@taylor.edu>
2015-11-12 18:29:53 -0500

    wrote karma test for .accounts .permissions .currentuser .update

--- karma/accounts/controllers/accountsCtrl.spec.js
+++ karma/accounts/controllers/accountsCtrl.spec.js
@@ -1,4 +1,45 @@
 describe('accountsCtrl', function(){
+  var controller,
+    scope,
+    mockModal,
+    mockAccountsService,
+		mockPermissionSetsService;
+
+  beforeEach(module("AccountsMod"));
+
+  beforeEach(inject(function($rootScope, $controller){
+    mockModal = jasmine.createSpyObj('mockModal', ['open']);
+
+    mockAccountsService = jasmine.createSpyObj('mockAccountsService', ['all','me','update','resendActivationEmail','destroy']);
+
+    mockPermissionSetsService = jasmine.createSpyObj('mockPermissionSetsService', ['all']);
+
+    scope = $rootScope.$new();
+    controller = $controller('AccountsCtrl', {
+      Accounts: mockAccountsService,
+      PermissionsSets: mockPermissionSetsService,
+      $modal: mockModal
+    });
+  }));
+
+  describe('on start', function(){
+    it('should get all accounts', function(){
+      expect(mockAccountsService.all).toHaveBeenCalled();
+    });
+
+    it('should get all permission sets', function(){
+      expect(mockPermissionSetsService.all).toHaveBeenCalled();
+    });
+
+    it('should get current user', function(){
+      expect(mockAccountsService.me).toHaveBeenCalled();
+    });
+
+    it('should update accounts', function(){
+      expect(mockAccountsService.update).toHaveBeenCalled();
+    });
+  });
+
 
 
 });
