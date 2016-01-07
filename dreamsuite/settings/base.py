c5641fbdf4c7c5e67aabf92f35c95272b2316351
Ben Duggan <benaduggan@gmail.com>
2016-01-06 21:54:02 -0500

    started a rudimentary version of containerized dreamsuite

--- dreamsuite/settings/base.py
+++ dreamsuite/settings/base.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'dreamsuite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 LANGUAGE_CODE = 'en-us'
 
