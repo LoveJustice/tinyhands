ff2f2f1c92e7944c6e2f8d67d259520407601f91
Ben Duggan <benaduggan@gmail.com>
2016-01-11 21:25:45 -0500

    cleaning up some docker stuff, no real changes

--- application/bin/run.sh
+++ application/bin/run.sh
@@ -3,10 +3,8 @@ python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
 
 # Prepare log files and start outputting logs to stdout
-
 tail -n 0 -f /srv/logs/*.log &
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn --config=gunicorn_config.py dreamsuite.wsgi:application
-# python manage.py runserver 0.0.0.0:9001
