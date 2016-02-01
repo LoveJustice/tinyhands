b150d655d0bf6cb0d2efb3a8f3e22721876eca85
s24r87 <s24r87@sbcglobal.net>
2016-02-01 10:11:08 -0500

    Changes to send IRF/VIF updates to google sheet when forms are submitted.

--- dreamsuite/settings/local.py
+++ dreamsuite/settings/local.py
@@ -4,3 +4,8 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 SITE_DOMAIN = 'localhost:8080'
+
+SPREADSHEET_NAME = 'Dream Suite - THN Data (Staging)'
+#SPREADSHEET_NAME = 'Tinyhands'
+IRF_WORKSHEET_NAME = 'IRF Entry'
+VIF_WORKSHEET_NAME = 'VIF Entry'
