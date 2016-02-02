3af42f170d96d1d475515eadbb333359d0a5bc8b
Ben Duggan <benaduggan@gmail.com>
2016-02-01 21:47:44 -0500

    organized files and updated dockerfiles accordingly. also updated the build script

--- application/bin/run.sh
+++ application/bin/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-python manage.py migrate                  # Apply database migrations
-python manage.py collectstatic --noinput  # Collect static files
+./manage.py migrate                  # Apply database migrations
+./manage.py collectstatic --noinput  # Collect static files
 
 # Prepare log files and start outputting logs to stdout
 tail -n 0 -f /srv/logs/*.log &
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-exec gunicorn --config=gunicorn_config.py dreamsuite.wsgi:application
+exec gunicorn --config=bin/gunicorn_config.py dreamsuite.wsgi:application
--- build-and-run.sh
+++
@@ -1,6 +0,0 @@
-docker build -t tusoftware-studio/dreamsuite-nginx ./build/nginx/
-docker build -t tusoftware-studio/dreamsuite-js-cli -f Dockerfile-js-cli .
-docker build -t tusoftware-studio/dreamsuite .
-
-docker-compose build
-docker-compose up -d
