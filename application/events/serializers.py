af6e9dcc3e249f6f747210be19f20439539c1316
Jacob Jin <yage_jin@taylor.edu>
2016-02-15 21:04:13 -0500

    get rid of the lines that not needed in serializers.py

--- application/events/serializers.py
+++ application/events/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from events.models import Event
 
 class EventsSerializer(serializers.ModelSerializer):
-    has_been_activated = serializers.BooleanField(source='has_usable_password',read_only=True)
 
     class Meta:
         model = Event
