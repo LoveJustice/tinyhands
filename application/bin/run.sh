1aefc9c5a1e16c1e5084c33c5b1cb2674deaa8f7
Ben Duggan <benaduggan@gmail.com>
2016-02-18 17:53:23 -0500

    added more budget endpoints from the budget module into the rest_api

--- application/bin/run.sh
+++ application/bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./manage.py migrate                  # Apply database migrations
-./manage.py collectstatic --noinput  # Collect static files
+./manage.py collectstatic -l --noinput  # Collect static files
 
 # Prepare log files and start outputting logs to stdout
 tail -n 0 -f /srv/logs/*.log &
--- application/rest_api/urls.py
+++ application/rest_api/urls.py
@@ -3,50 +3,64 @@ from accounts.views import CurrentUserView
 
 from rest_framework.authtoken import views
 
-from budget.views import BudgetViewSet, OtherItemsViewSet
+from budget.views import BudgetViewSet, OtherItemsViewSet, MoneyDistribution, MoneyDistributionFormPDFView, money_distribution_view, retrieve_latest_budget_sheet_for_border_station, previous_data, StaffSalaryViewSet
+
 from dataentry.views import Address2ViewSet, Address1ViewSet, GeoCodeDistrictAPIView, GeoCodeVdcAPIView, InterceptionRecordViewSet, VictimInterviewViewSet
 from portal.views import *
 from accounts.views import AccountViewSet, DefaultPermissionsSetViewSet, CurrentUserView, ResendActivationEmailView
 from static_border_stations.views import BorderStationViewSet, StaffViewSet, CommitteeMemberViewSet, LocationViewSet
 
 
+list = {'get': 'list', 'post': 'create'}
+detail = {'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}
+
+
 urlpatterns = patterns('rest_api.views',
     # Addresses
-    url(r'^address1/$', Address1ViewSet.as_view({'get': 'list', 'post': 'create'}), name='Address1'),
+    url(r'^address1/$', Address1ViewSet.as_view(list), name='Address1'),
     url(r'^address1/all/$', Address1ViewSet.as_view({'get': 'list_all'}), name='Address1all'),
-    url(r'^address1/(?P<pk>\d+)/$', Address1ViewSet.as_view({'put': 'update', 'get': 'retrieve', 'delete': 'destroy'}), name='Address1detail'),
+    url(r'^address1/(?P<pk>\d+)/$', Address1ViewSet.as_view(detail), name='Address1detail'),
 
-    url(r'^address2/$', Address2ViewSet.as_view({'get': 'list', 'post': 'create'}), name='Address2'),
-    url(r'^address2/(?P<pk>\d+)/$', Address2ViewSet.as_view({'put': 'update', 'get': 'retrieve', 'delete': 'destroy'}), name='Address2detail'),
+    url(r'^address2/$', Address2ViewSet.as_view(list), name='Address2'),
+    url(r'^address2/(?P<pk>\d+)/$', Address2ViewSet.as_view(detail), name='Address2detail'),
 
     # Fuzzy searching for addresses
     url(r'^address1/fuzzy/$', GeoCodeDistrictAPIView.as_view(), name="Address1FuzzySearch"),
     url(r'^address2/fuzzy/$', GeoCodeVdcAPIView.as_view(), name="Address2FuzzySearch"),
 
     # Budget URLs
-    url(r'^budget/$', BudgetViewSet.as_view({'get': 'list', 'post': 'create'}), name='BudgetCalculation'),
-    url(r'^budget/(?P<pk>\d+)/$', BudgetViewSet.as_view({'put': 'update', 'get': 'retrieve', 'delete': 'destroy'}), name='BudgetCalculationWithId'),
+    url(r'^budget/$', BudgetViewSet.as_view(list), name='BudgetCalculation'),
+    url(r'^budget/(?P<pk>\d+)/$', BudgetViewSet.as_view(list), name='BudgetCalculationWithId'),
 
-    # Other items
     url(r'^budget/(?P<parent_pk>\d+)/item/$', OtherItemsViewSet.as_view({'get': 'list_by_budget_sheet', 'post': 'create'}), name='BudgetCalculationWithId'),
-    url(r'^budget/(?P<parent_pk>\d+)/item/(?P<pk>\d+)/$', OtherItemsViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='BudgetCalculationWithId'),
+    url(r'^budget/(?P<parent_pk>\d+)/item/(?P<pk>\d+)/$', OtherItemsViewSet.as_view(detail), name='BudgetCalculationWithId'),
+
+    url(r'^budget/most_recent_form/(?P<pk>\d+)/$', retrieve_latest_budget_sheet_for_border_station, name="rest_api_budget_new_api"),
+    url(r'^budget/previous_data/(?P<pk>\d+)/(?P<month>\d+)/(?P<year>\d+)/$', previous_data, name="rest_api_previous_data"),
+
+    url(r'^budget/staff_salary/$', StaffSalaryViewSet.as_view(list), name="rest_api_staff_salary_list_api"),
+    url(r'^budget/staff_salary/(?P<pk>\d+)/$', StaffSalaryViewSet.as_view({'get': 'budget_calc_retrieve', 'put': 'update', 'delete': 'destroy'}), name="rest_api_staff_salary_detail_api"),
+
+    url(r'^budget/money_distribution/(?P<pk>\d+)/$', MoneyDistribution.as_view({'get': 'get_people_needing_form', 'post': 'send_emails'}), name="rest_api_money_distribution_api"),
+    url(r'^budget/money_distribution_pdf/(?P<pk>\d+)/$', MoneyDistributionFormPDFView.as_view(), name="rest_api_money_distribution_pdf"),
+    url(r'^budget/money_distribution/view/(?P<pk>\d+)/$', money_distribution_view, name="rest_api_money_distribution_view"),
 
 
     # BorderStation URLs
     url(r'^border-station/$', BorderStationViewSet.as_view({'get':'list_all', 'post':'create'}), name="BorderStations"), # Detail
-    url(r'^border-station/(?P<pk>\d+)/$', BorderStationViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'}), name="BorderStation"),
+    url(r'^border-station/(?P<pk>\d+)/$', BorderStationViewSet.as_view(detail), name="BorderStation"),
 
     # Committee Members
-    url(r'^committee-member/$', CommitteeMemberViewSet.as_view({'get':'list', 'post':'create'}), name="CommitteeMembers"),
-    url(r'^committee-member/(?P<pk>\d+)/$', CommitteeMemberViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'}), name="CommitteeMember"),
+    url(r'^committee-member/$', CommitteeMemberViewSet.as_view(list), name="CommitteeMembers"),
+    url(r'^committee-member/(?P<pk>\d+)/$', CommitteeMemberViewSet.as_view(detail), name="CommitteeMember"),
 
     # Locations
-    url(r'^location/$', LocationViewSet.as_view({'get':'list', 'post':'create'}), name="Locations"),
-    url(r'^location/(?P<pk>\d+)/$', LocationViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'}), name="Location"),
+    url(r'^location/$', LocationViewSet.as_view(list), name="Locations"),
+    url(r'^location/(?P<pk>\d+)/$', LocationViewSet.as_view(detail), name="Location"),
 
     # Staff
-    url(r'^staff/$', StaffViewSet.as_view({'get':'list', 'post':'create'}), name="AllStaff"),
-    url(r'^staff/(?P<pk>\d+)/$', StaffViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'}), name="Staff"),
+    url(r'^staff/$', StaffViewSet.as_view(list), name="AllStaff"),
+    url(r'^staff/(?P<pk>\d+)/$', StaffViewSet.as_view(detail), name="Staff"),
 
     # Dashboard/Portal
     url(r'^get_interception_records/$', get_interception_records, name='get_interception_records'),
@@ -54,13 +68,13 @@ urlpatterns = patterns('rest_api.views',
     #Accounts and DefaultPermissionsSets
     url(r'^me/$', CurrentUserView.as_view(), name="CurrentUser"),
 
-    url(r'^account/$', AccountViewSet.as_view({'get': 'list', 'post':'create'}), name="AccountList"),
+    url(r'^account/$', AccountViewSet.as_view(list), name="AccountList"),
     url(r'^account/all/$', AccountViewSet.as_view({'get': 'list_all'}), name="AccountListAll"),
-    url(r'^account/(?P<pk>\d+)/$', AccountViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'}), name='Account'),
+    url(r'^account/(?P<pk>\d+)/$', AccountViewSet.as_view(detail), name='Account'),
     url(r'^account/resend-activation-email/(?P<pk>\d+)/$', ResendActivationEmailView.as_view(), name='ResendActivationEmail'),
 
-    url(r'^defaultPermissionsSet/$', DefaultPermissionsSetViewSet.as_view({'get': 'list', 'post':'create'}), name="DefaultPermissionsSets"),
-    url(r'^defaultPermissionsSet/(?P<pk>\d+)/$', DefaultPermissionsSetViewSet.as_view({'get': 'retrieve', 'put':'update', 'delete':'destroy'}), name="DefaultPermissionsSet"),
+    url(r'^defaultPermissionsSet/$', DefaultPermissionsSetViewSet.as_view(list), name="DefaultPermissionsSets"),
+    url(r'^defaultPermissionsSet/(?P<pk>\d+)/$', DefaultPermissionsSetViewSet.as_view(detail), name="DefaultPermissionsSet"),
 
     # IRFs
     url(r'^irf/$', InterceptionRecordViewSet.as_view({'get': 'list'}), name="InterceptionRecord"),
