30f317c1c17a41e0cce75bb10b063f5ef4cf545b
kevoconnor <kevin_oconnor@taylor.edu>
2016-02-27 19:30:43 -0500

    Editing sysAdminCtrl.js for sys_admin_settings form

--- application/dataentry/static/dataentry/js/controllers/sysAdminCtrl.js
+++ application/dataentry/static/dataentry/js/controllers/sysAdminCtrl.js
@@ -1,11 +1,33 @@
 angular
     .module('DataEntry')
-    .controller("sysAdminCtrl", ['$scope', function($scope){
-        var vm = this;
+    .controller("sysAdminCtrl", function($scope) {
 
-        vm.form = {};
+        // Variable Declarations
+        var form = {
+            address1_cutoff = 0,
+            address1_limit = 0,
+            address2_cutoff = 0,
+            address2_limit = 0,
+            person_cutoff = 0,
+            person_limit = 0,
+            phone_number_cutoff = 0,
+            phone_number_limit = 0
+        };
+        // vm.address1_cutoff = 0;
+        // vm.address1_limit = 0;
+        // vm.address2_cutoff = 0;
+        // vm.address2_limit = 0;
+        // vm.person_cutoff = 0;
+        // vm.person_limit = 0;
+        // vm.phone_number_cutoff = 0;
+        // vm.phone_number_limit = 0;
 
-        vm.locationTotal = function() {
-            return vm.form.location_value;
-        }
-    }]);
+        // Function Declarations
+        //vm.updateForm = updateForm;
+
+
+        $scope.updateForm = function() {
+            form = $scope.form;
+            console.log(form);
+        };
+    });
