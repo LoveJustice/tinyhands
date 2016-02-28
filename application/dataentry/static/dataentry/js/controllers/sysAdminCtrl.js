6faa5fbb059c0530fcbc7fdfa07f943fc9a55c95
kevoconnor <kevin_oconnor@taylor.edu>
2016-02-27 20:46:39 -0500

    Added limit column to Fuzzy Matching Settings section, removed comments from sysAdminCtrl.js and edited controller

--- application/dataentry/static/dataentry/js/controllers/sysAdminCtrl.js
+++ application/dataentry/static/dataentry/js/controllers/sysAdminCtrl.js
@@ -1,11 +1,23 @@
+'use strict';
+
 angular
     .module('DataEntry')
-    .controller("sysAdminCtrl", ['$scope', function($scope){
-        var vm = this;
+    .controller("sysAdminCtrl", function($scope) {
 
-        vm.form = {};
+        // Variable Declarations
+        var form = {
+            address1_cutoff = 0,
+            address1_limit = 0,
+            address2_cutoff = 0,
+            address2_limit = 0,
+            person_cutoff = 0,
+            person_limit = 0,
+            phone_number_cutoff = 0,
+            phone_number_limit = 0
+        };
 
-        vm.locationTotal = function() {
-            return vm.form.location_value;
-        }
-    }]);
+        $scope.updateForm = function() {
+            form = $scope.form;
+            console.log(form);
+        };
+    });
