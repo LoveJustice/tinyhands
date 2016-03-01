6769175070a3a642ee10b81f03b16598faf2215b
amun33 <amunn33@gmail,com>
2016-02-29 23:19:24 -0500

    Removed spec function that was not being used

--- application/e2etesting/accounts/accountList.js
+++ application/e2etesting/accounts/accountList.js
@@ -15,10 +15,6 @@ var accountListPage = function() {
     element(by.id(id.toString())).element(by.id('edit')).click();
   };
 
-  this.checkUserDesignation = function(id) {
-    designation = element(by.id(id.toString())).element(by.tagName("td")).element(by.buttonText('Super Administrator'));
-    return designation;
-  };
 
   this.navigateToDeleteModal = function(id) {
     element(by.id(id.toString())).element(by.buttonText('Delete')).click();
