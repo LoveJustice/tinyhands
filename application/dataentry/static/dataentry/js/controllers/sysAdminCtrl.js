cf199713a2170c0884fb67b12cd348437e562146
kevoconnor <kevin_oconnor@taylor.edu>
2016-03-01 15:47:47 -0500

    Edited sys_admin_settings template and fixed errors

--- application/dataentry/static/dataentry/js/controllers/sysAdminCtrl.js
+++ application/dataentry/static/dataentry/js/controllers/sysAdminCtrl.js
@@ -2,22 +2,23 @@
 
 angular
     .module('DataEntry')
-    .controller("sysAdminCtrl", function($scope) {
+    .controller("sysAdminCtrl", ['$scope','$http','$timeout', function($scope, $http, $timeout) {
 
         // Variable Declarations
-        var form = {
-            address1_cutoff = 0,
-            address1_limit = 0,
-            address2_cutoff = 0,
-            address2_limit = 0,
-            person_cutoff = 0,
-            person_limit = 0,
-            phone_number_cutoff = 0,
-            phone_number_limit = 0
-        };
+        // var form = {
+        //     address1_cutoff = 0,
+        //     address1_limit = 0,
+        //     address2_cutoff = 0,
+        //     address2_limit = 0,
+        //     person_cutoff = 0,
+        //     person_limit = 0,
+        //     phone_number_cutoff = 0,
+        //     phone_number_limit = 0
+        // };
+        var form = {};
 
         $scope.updateForm = function() {
             form = $scope.form;
             console.log(form);
         };
-    });
+    }]);
