c5908123c37296225344ec62d6fed98d3998dbb0
Ben Duggan <benaduggan@gmail.com>
2016-03-02 10:15:52 -0500

    I was stupid... I had the wrong env vars going into the wrong vars and it broke everything =P

--- application/dreamsuite/settings/production.py
+++ application/dreamsuite/settings/production.py
@@ -12,8 +12,8 @@ SITE_DOMAIN = 'tinyhandsdreamsuite.org'
 # We use S3 in production to serve media files so these are configuration variables for accessing that
 AWS_QUERYSTRING_AUTH = False
 AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
-AWS_ACCESS_KEY_ID = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 
@@ -22,7 +22,10 @@ MEDIAFILES_LOCATION = 'media'
 
 # STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+
 from dataentry.custom_storages import *
+
 DEFAULT_FILE_STORAGE = 'dataentry.custom_storages.MediaStorage'
 # STATICFILES_STORAGE = 'dataentry.custom_storages.StaticStorage'
 
