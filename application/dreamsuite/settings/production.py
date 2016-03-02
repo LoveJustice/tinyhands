4819079dfc07e1abd2ed800793298a851475d261
Ben Duggan <benaduggan@gmail.com>
2016-03-01 23:36:31 -0500

    able to switch static and media file locations to upload them to s3 or keep them locally based on the environment

--- application/dreamsuite/settings/production.py
+++ application/dreamsuite/settings/production.py
@@ -1,30 +1,30 @@
 from .base import *
 
-STATICFILES_LOCATION = 'static'
-MEDIAFILES_LOCATION = 'media'
 
 
 DEBUG = True
 
-ADMINS = (('Tom Nurkkala', 'tnurkkala@cse.taylor.edu'),('Austin Munn', 'austin_munn@taylor.edu'))
+ADMINS = (('Ben Duggan', 'benjamin_duggan@taylor.edu'))
 
 SITE_DOMAIN = 'tinyhandsdreamsuite.org'
 
+
+# We use S3 in production to serve media files so these are configuration variables for accessing that
 AWS_QUERYSTRING_AUTH = False
-AWS_STORAGE_BUCKET_NAME = 'dreamsuite-media'
+AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
-# AWS_S3_CUSTOM_DOMAIN = 'https://s3-ap-southeast-1.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
-
-
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 from dataentry.custom_storages import *
 DEFAULT_FILE_STORAGE = 'dataentry.custom_storages.MediaStorage'
-STATICFILES_STORAGE = 'dataentry.custom_storages.StaticStorage'
+# STATICFILES_STORAGE = 'dataentry.custom_storages.StaticStorage'
 
 SPREADSHEET_NAME = 'Dream Suite - THN Data'
 IRF_WORKSHEET_NAME = 'IRF Entry'
