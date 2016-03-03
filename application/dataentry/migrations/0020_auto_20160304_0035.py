d2129b4bb64cb92a8f5754bd7db9f013e022bcad
amun33 <amunn33@gmail,com>
2016-03-03 14:26:18 -0500

    Added final two migrations

---
+++ application/dataentry/migrations/0020_auto_20160304_0035.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dataentry', '0019_auto_20160304_0034'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='district',
+            name='level',
+            field=models.CharField(default=b'District', max_length=255, choices=[(b'State', b'State'), (b'Country', b'Country'), (b'City', b'City'), (b'District', b'District'), (b'VDC', b'VDC'), (b'Building', b'Building'), (b'Block', b'Block')]),
+            preserve_default=True,
+        ),
+    ]
