c4b30f36e2ddfd7a189fc1b956cf9a9e605b9794
amun33 <amunn33@gmail,com>
2016-03-03 15:00:35 -0500

    Squashed migrations

--- application/dataentry/migrations/0015_auto_20160304_0144.py
+++ application/dataentry/migrations/0015_auto_20160304_0144.py
@@ -13,6 +13,12 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='district',
+            name='completed',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='district',
             name='latitude',
             field=models.FloatField(default=0),
             preserve_default=True,
@@ -20,7 +26,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='district',
             name='level',
-            field=models.CharField(default=b'District', max_length=255),
+            field=models.CharField(default=b'District', max_length=255, choices=[(b'State', b'State'), (b'Country', b'Country'), (b'City', b'City'), (b'District', b'District'), (b'VDC', b'VDC'), (b'Building', b'Building'), (b'Block', b'Block')]),
             preserve_default=True,
         ),
         migrations.AddField(
