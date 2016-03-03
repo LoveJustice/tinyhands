d1ac3f95a3655e49248ba6bda428a53945e51a21
amun33 <amunn33@gmail,com>
2016-03-03 15:37:42 -0500

    Merged my migration with Jon's migrations

--- application/dataentry/migrations/0015_auto_20160304_0144.py
+++ application/dataentry/migrations/0015_auto_20160304_0144.py
@@ -7,30 +7,30 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dataentry', '0014_auto_20151221_2247'),
+        ('dataentry', '0028_auto_20160221_0004'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='district',
+            model_name='Address1',
             name='completed',
             field=models.BooleanField(default=False),
             preserve_default=True,
         ),
         migrations.AddField(
-            model_name='district',
+            model_name='Address1',
             name='latitude',
             field=models.FloatField(default=0),
             preserve_default=True,
         ),
         migrations.AddField(
-            model_name='district',
+            model_name='Address1',
             name='level',
             field=models.CharField(default=b'District', max_length=255, choices=[(b'State', b'State'), (b'Country', b'Country'), (b'City', b'City'), (b'District', b'District'), (b'VDC', b'VDC'), (b'Building', b'Building'), (b'Block', b'Block')]),
             preserve_default=True,
         ),
         migrations.AddField(
-            model_name='district',
+            model_name='Address1',
             name='longitude',
             field=models.FloatField(default=0),
             preserve_default=True,
