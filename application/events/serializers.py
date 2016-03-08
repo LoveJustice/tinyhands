c864dfa6fe22fab9095fca0f6f9ca2816c79aeef
Andrew Smith <videogame0201@gmail.com>
2016-03-08 17:08:22 -0500

    Added date and time validation to EventsSerializer

--- application/events/serializers.py
+++ application/events/serializers.py
@@ -5,3 +5,22 @@ class EventsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
+
+    def validate(self, data):
+        if data['is_repeat'] and not data['repetition']:
+            raise serializers.ValidationError('Repetition is required if event is repeated.')
+
+        start_date = data['start_date']
+        end_date = data['end_date']
+        start_time = data['start_time']
+        end_time = data['end_time']
+        ends = data['ends']
+        if start_date and end_date:
+            if start_date > end_date:
+                raise serializers.ValidationError('Start date is not allowed to be greater than end date.')
+            if start_date == end_date:
+                if start_time > end_time:
+                    raise serializers.ValidationError('Start time must less than end time for same day.')
+        if ends and ends <= start_date:
+            raise serializers.ValidationError('Events repetition ends must be greater than first event end date.')
+        return data
