8a7776cc9199aad820bc203f897ef4c3a0eea621
Ben Duggan <benaduggan@gmail.com>
2016-03-10 16:18:04 -0500

    breakup script into two

--- build-containers.sh
+++ build-containers.sh
@@ -4,11 +4,3 @@ cp -a application/media build/nginx/
 
 docker build -t tusoftwarestudio/dreamsuite-nginx ./build/nginx/
 docker build -t tusoftwarestudio/dreamsuite .
-
-docker push tusoftwarestudio/dreamsuite-nginx
-docker push tusoftwarestudio/dreamsuite
-
-# docker push tusoftwarestudio/dreamsuite-js-cli
-# docker build -t tusoftwarestudio/dreamsuite-js-cli -f Dockerfile-js-cli .
-
-
