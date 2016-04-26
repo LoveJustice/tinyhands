2dc489632fc48c86acb0b054bd8a2b757a8814fd
bsell <bryantsell@gmail.com>
2016-04-26 16:57:02 -0400

    Add validation to irf serializer to throw warnings. Need to add ability to ignore warnings with a flag. Need to add additional errors to validation.

--- application/dataentry/serializers.py
+++ application/dataentry/serializers.py
@@ -91,6 +91,69 @@ class InterceptionRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = InterceptionRecord
 
+    def validate(self, data):
+        validation_response = {}
+        errors = self.check_for_errors(data)
+        warnings = self.check_for_warnings(data)
+        if len(errors) > 0:
+            validation_response['errors'] = errors
+        if len(warnings) > 0:
+            validation_response['warnings'] = warnings
+        if len(warnings) > 0 or len(errors) > 0:
+            raise serializers.ValidationError(validation_response)
+        return data
+
+    def check_for_errors(self, data): # For validation
+        errors = []
+        return errors
+
+    def check_for_red_flags(self, data): # For validation
+        any_red_flags = False
+        for field in InterceptionRecord._meta.fields:
+            try:
+                if field.weight:
+                    if data[field.name] == True:
+                        any_red_flags = True
+            except:
+                pass
+        if any_red_flags == False:
+            return [{'red_flags': 'No red flags are checked.'}]
+        return []
+
+    def check_for_warnings(self, data): # For validation
+        warnings = []
+        warnings += self.check_for_red_flags(data)
+        warnings += self.check_procedure(data)
+        warnings += self.check_type(data)
+        if data['has_signature'] == False:
+            warnings.append({'has_signature': 'Form should be signed, though not required.'})
+        return warnings
+
+
+    def check_procedure(self, data): # For validation
+        warnings = []
+        if data['call_thn_to_cross_check'] == False:
+            warnings.append({'call_thn_to_cross_check': 'Procedure not followed.'})
+        if data['call_subcommittee_chair'] == False:
+            warnings.append({'call_subcommittee_chair': 'Procedure not followed.'})
+        if data['scan_and_submit_same_day'] == False:
+            warnings.append({'scan_and_submit_same_day': 'Procedure not followed.'})
+        return warnings
+
+    def check_type(self, data): # For validation
+        type_is_set = False
+        for field in InterceptionRecord._meta.fields:
+            try:
+                if field.name:
+                    if 'type' in field.name and data[field.name] == True:
+                        type_is_set = True
+            except:
+                pass
+        if type_is_set == False:
+            return [{'interception_type': 'Field should be included, though not required.'}]
+        return []
+
+
 
 
 class VictimInterviewListSerializer(serializers.ModelSerializer):
