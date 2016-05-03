8f0c733f534484a74eff677adeb69ec27b1b7daf
Benjamin Duggan <benaduggan@users.noreply.github.com>
2016-05-03 09:27:27 -0400

    Update run.sh
    Just deleting an unnecessary space

--- application/bin/run.sh
+++ application/bin/run.sh
@@ -3,4 +3,4 @@ python /data/manage.py migrate --noinput        # Apply database migrations
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-exec gunicorn --config=bin/gunicorn_config.py dreamsuite.wsgi:application 
+exec gunicorn --config=bin/gunicorn_config.py dreamsuite.wsgi:application
