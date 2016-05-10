ca4f4d8580fdceb1f10ac17ab77a7b432ab12d19
Andrew Jackson <andrew.c.j1995@gmail.com>
2016-05-10 16:32:40 -0400

    Added tests for both delete and get for vif_rest_serializers_validation

--- application/dataentry/tests/test_vif_apiview.py
+++ application/dataentry/tests/test_vif_apiview.py
@@ -1,19 +1,25 @@
 from django.core.urlresolvers import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
-from accounts.tests.factories import BadVifUserFactory, SuperUserFactory
+from rest_framework.test import APITestCase, APIClient
+from accounts.tests.factories import BadVifUserFactory, SuperUserFactory,ViewUserFactory, NoPermissionUserFactory
 from dataentry.tests.factories import VifFactory
 
+class RestApiTestCase(APITestCase):
 
-class VifTest(APITestCase):
+    def login(self, user):
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+class VifTest(RestApiTestCase):
     def setUp(self):
         self.Vif_list = VifFactory.create_batch(20)
-        self.user = SuperUserFactory.create()
-        self.client.force_authenticate(user=self.user)
+        #self.user = SuperUserFactory.create()
+        #self.client.force_authenticate(user=self.user)
 
     def test_list_vif(self):
         url = reverse('VictimInterview')
-
+        user = SuperUserFactory.create()
+        self.login(user)
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -32,3 +38,66 @@ class VifTest(APITestCase):
             url = reverse('VictimInterviewDetail',  args=[self.Vif_list[0].id])
             response = self.client.delete(url)
             self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+            
+            
+class VifTestDestroy(RestApiTestCase):     
+    def test_when_not_authenticated_should_deny_access(self):
+        
+        vif_to_delete = VifFactory.create()
+        url = reverse('VictimInterviewDetail',args=[vif_to_delete.id])
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_when_authenticated_and_does_not_have_permission_should_deny_access(self):
+        user = ViewUserFactory.create()
+        vif_to_delete = VifFactory.create()
+        url = reverse('VictimInterviewDetail',args=[vif_to_delete.id])
+        self.login(user)
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], 'You do not have the right permission to access this data')
+
+    def test_when_authenticated_and_has_permission_should_delete_vif(self):
+        user = SuperUserFactory.create()
+        vif_to_delete = VifFactory.create()
+        url = reverse('VictimInterviewDetail',args=[vif_to_delete.id])
+        self.login(user)
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+class VifTestGet(RestApiTestCase):
+    def test_when_not_authenticated_should_deny_access(self):
+        vif_to_get = VifFactory.create()
+        url = reverse('VictimInterviewDetail',args=[vif_to_get.id])
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_when_authenticated_has_permission_should_allow_access(self):
+        user = ViewUserFactory.create()
+        vif_to_get = VifFactory.create()
+        url = reverse('VictimInterviewDetail',args=[vif_to_get.id])
+        self.login(user)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_when_authenticated_does_not_have_permission_should_deny_access(self):
+        user = NoPermissionUserFactory.create()
+        vif_to_get = VifFactory.create()
+        url = reverse('VictimInterviewDetail',args=[vif_to_get.id])
+        self.login(user)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
