d6c6bc5a8ea50a70e6e6b99e741b8799a90a270e
Andrew Jackson <andrew.c.j1995@gmail.com>
2016-05-10 20:57:42 -0400

    Work on unit tests for endpoints. Some working. Some not. Abram also worked too.

--- application/dataentry/tests/test_vif_apiview.py
+++ application/dataentry/tests/test_vif_apiview.py
@@ -72,7 +72,34 @@ class VifTestDestroy(RestApiTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-class VifTestGet(RestApiTestCase):
+class VifTestGet_list(RestApiTestCase):
+    def test_when_not_authenticated_should_deny_access(self):
+        url = reverse('VictimInterview')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_when_authenticated_has_permission_should_allow_access(self):
+        user = ViewUserFactory.create()
+        url = reverse('VictimInterview')
+        self.login(user)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_when_authenticated_does_not_have_permission_should_deny_access(self):
+        user = NoPermissionUserFactory.create()
+        url = reverse('VictimInterview')
+        self.login(user)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class VifTestGet_Details(RestApiTestCase):     
     def test_when_not_authenticated_should_deny_access(self):
         vif_to_get = VifFactory.create()
         url = reverse('VictimInterviewDetail',args=[vif_to_get.id])
@@ -101,3 +128,41 @@ class VifTestGet(RestApiTestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+class VifTestPut(RestApiTestCase):
+
+    def get_updated_vif(self, vif, new_name):
+        return {
+            "id": vif.id,
+            "vif_number": vif.vif_number,
+            "interviewer": new_name,
+            "number_of_victims": vif.number_of_victims,
+            "number_of_traffickers": vif.number_of_traffickers,
+            "date": vif.date,
+            "date_time_entered_into_system": vif.date_time_entered_into_system,
+            "date_time_last_updated": vif.date_time_last_updated,
+            "victim_name" : new_name,
+            "victim_gender" : "M"
+        }
+
+
+    def test_when_not_authenticated_should_deny_access(self):
+        vif_to_put = VifFactory.create()
+        url = reverse('VictimInterviewDetail',args=[vif_to_put.id])
+
+        response = self.client.put(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)     
+    
+    #This is not working needs more tests
+    def test_when_authenticated_and_does_not_have_permission_should_allow_access(self):
+        user = SuperUserFactory.create()         
+        vif_to_put = VifFactory.create()
+        url = reverse('VictimInterviewDetail',args=[vif_to_put.id])
+        self.login(user)
+        
+        new_irf = self.get_updated_vif(vif_to_put, "Fred")
+        
+        response = self.client.put(url, new_irf)
+
+        self.assertEqual(response.data, status.HTTP_403_FORBIDDEN)
