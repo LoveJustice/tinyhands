7bf7ac7ffa4009db79b642f38a3a106937451ced
Ben Duggan <benaduggan@gmail.com>
2016-05-10 22:21:00 -0400

    syncing media from production

---
+++ application/bin/rsync-media-from-production.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+RS='mkdir --parents'
+
+RSYNC='rsync --verbose --archive --progress --rsh=ssh'
+
+THI_PROD_MEDIA='thi-production:/home/thi/tinyhands/media'
+PROJECT_DIR='/home/thi/tinyhands/'
+
+$RSYNC $THI_PROD_MEDIA $PROJECT_DIR
