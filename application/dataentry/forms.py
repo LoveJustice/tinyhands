efc9505fde238bf0f514cb72f3d1c6f63d2821a8
Ben Duggan <benaduggan@gmail.com>
2016-05-12 12:37:36 -0400

    I believe I have fixed the vif problems with getting multiple addresses.

--- application/dataentry/forms.py
+++ application/dataentry/forms.py
@@ -746,15 +746,15 @@ class VictimInterviewForm(DreamSuitePaperForm):
         if self.cleaned_data['victim_address1']:
             victim_address1 = Address1.objects.get(name=self.cleaned_data['victim_address1'])
             self.instance.victim.address1 = victim_address1
-        if self.cleaned_data['victim_guardian_address1']:
-            victim_guardian_address1 = Address1.objects.get(name=self.cleaned_data['victim_guardian_address1'])
-            self.instance.victim_guardian_address1 = victim_guardian_address1
-            print victim_address1.id
         if self.cleaned_data['victim_address2']:
             victim_address2 = Address2.objects.get(name=self.cleaned_data['victim_address2'], address1_id = self.instance.victim.address1.id)
             self.instance.victim.address2 = victim_address2
+
+        if self.cleaned_data['victim_guardian_address1']:
+            victim_guardian_address1 = Address1.objects.get(name=self.cleaned_data['victim_guardian_address1'])
+            self.instance.victim_guardian_address1 = victim_guardian_address1
         if self.cleaned_data['victim_guardian_address2']:
-            victim_guardian_address2 = Address2.objects.get(name=self.cleaned_data['victim_guardian_address2'])
+            victim_guardian_address2 = Address2.objects.get(name=self.cleaned_data['victim_guardian_address2'], address1=victim_guardian_address1)
             self.instance.victim_guardian_address2 = victim_guardian_address2
 
         if self.cleaned_data["victim_name"]:
