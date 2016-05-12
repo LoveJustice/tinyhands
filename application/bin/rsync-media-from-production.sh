25877899567fd723ebcfb9b61c34e20c9359385e
Ben Duggan <benaduggan@gmail.com>
2016-05-12 10:33:01 -0400

    updates for rsync and photo null allowed

--- application/bin/rsync-media-from-production.sh
+++ application/bin/rsync-media-from-production.sh
@@ -7,6 +7,6 @@ RS='mkdir --parents'
 RSYNC='rsync --verbose --archive --progress --rsh=ssh'
 
 THI_PROD_MEDIA='thi-production:/home/thi/tinyhands/media'
-PROJECT_DIR='/home/thi/tinyhands/'
+PROJECT_DIR='/data/'
 
 $RSYNC $THI_PROD_MEDIA $PROJECT_DIR
