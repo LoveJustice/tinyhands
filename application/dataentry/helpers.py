7ec08db336b7efb2a0535ae1fea514c39ef86869
Ben Duggan <benaduggan@gmail.com>
2016-08-18 10:13:30 -0400

    locationboxes now return ids of vif

--- application/dataentry/helpers.py
+++ application/dataentry/helpers.py
@@ -17,6 +17,13 @@ def related_items_helper(self, obj):
     ]
 
     for relationship in relationships:
+        if relationship.related_model._meta.model_name =='victiminterviewlocationbox':
+            related_items_list.append({
+                "type": relationship.related_model._meta.model_name,
+                "ids": [model.victim_interview_id for model in relationship.related_model.objects.filter(**{relationship.remote_field.name: obj})]
+            })
+            continue
+
         related_items_list.append({
             "type": relationship.related_model._meta.model_name,
             "ids": [model.id for model in relationship.related_model.objects.filter(**{relationship.remote_field.name: obj})]
