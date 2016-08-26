5d5f2b79c9be8364dce31a71e3069e452eb34c6b
Ben Duggan <benaduggan@gmail.com>
2016-08-26 17:34:38 -0400

    refactoring names a little to be more python-y

--- application/dataentry/models/site_settings.py
+++ application/dataentry/models/site_settings.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
+
 class FuzzyMatching(models.Model):
     address1_cutoff = models.PositiveIntegerField(default=70)
     address1_limit = models.PositiveIntegerField(default=5)
@@ -11,6 +12,7 @@ class FuzzyMatching(models.Model):
     # PHONE NUMBER MATCHING FOR FUTURE USE
     # phone_number_cutoff = models.PositiveIntegerField(default=0)
     # phone_number_limit = models.PositiveIntegerField(default=0)
+
 
 class SiteSettings(models.Model):
     data = JSONField()
--- application/rest_api/urls.py
+++ application/rest_api/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
         url(r'^interceptee/(?P<pk>\d+)/$', IntercepteeViewSet.as_view(detail), name="IntercepteeDetail"),
 
         #SiteSettings
-        url(r'^SiteSettings/$', SiteSettingsViewSet.as_view({'get': 'retrieve_custom', 'put': 'update'}), name="SiteSettings"),
+        url(r'^site-settings/$', SiteSettingsViewSet.as_view({'get': 'retrieve_custom', 'put': 'update'}), name="SiteSettings"),
 
         # VIFs
         url(r'^vif/$', VictimInterviewViewSet.as_view(list), name="VictimInterview"),
