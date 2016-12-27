440d24cebc82e7bc58fe35749d742452ca2926e0
Ben Duggan <benaduggan@gmail.com>
2016-12-26 23:54:14 -0500

    added a couple more tests

--- application/dataentry/tests/test_irfvif_exists_views.py
+++ application/dataentry/tests/test_irfvif_exists_views.py
@@ -20,6 +20,16 @@ class IrfExistsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, irf_number)
 
+    def test_irfexists_allows_optional_letters_after_the_number(self):
+        irf_number = "BHD123BA"
+        irf = IrfFactory.create(irf_number="BHD123BA")
+        url = reverse('IrfExists', args=[irf_number])
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, irf_number)
+
     def test_irfexists_returns_error_if_doesnt_exist(self):
         url = reverse('IrfExists', args=["BHD123ASDASD"])
 
@@ -44,6 +54,16 @@ class VifExistsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, vif_number)
 
+    def test_vifexists_allows_optional_letters_after_the_number(self):
+        vif_number = "BHD123BA"
+        vif = VifFactory.create(vif_number="BHD123BA")
+        url = reverse('VifExists', args=[vif_number])
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, vif_number)
+
     def test_vifexists_returns_error_if_doesnt_exist(self):
         url = reverse('VifExists', args=["BHD123ASDASD"])
 
@@ -51,3 +71,4 @@ class VifExistsTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, "Vif does not exist")
+
