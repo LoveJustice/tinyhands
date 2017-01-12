f1879af60599cd8a0bbafd31a08545f77c481578
Ben Duggan <benaduggan@gmail.com>
2017-01-11 20:22:28 -0500

    try changing up the config a bit. it seems like the env isn't being filled in by the encrypted args

--- build/deploy/run.sh
+++ build/deploy/run.sh
@@ -1,8 +1,11 @@
 #/bin/bash
 
+
 echo $CI_COMMIT_ID > /dreamsuite_tag
 echo -e $PRIVATE_SSH_KEY > /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
+
+env
 
 if [ "$CI_BRANCH" = "master" ]
 then
@@ -13,7 +16,7 @@ fi
 
 echo $CI_BRANCH
 echo copy over dreamsuite tag $CI_COMMIT_ID to $HOST
-scp -rp /dreamsuite_tag thi@$HOST:/home/thi/tinyhands/dreamsuite_tag
+scp -v /dreamsuite_tag thi@$HOST:/home/thi/tinyhands/dreamsuite_tag
 
 echo execute run script
 ssh thi@staging.tinyhandsdreamsuite.org 'tinyhands/run.sh'
