8bf92748da6168b1bb594c63b2202b7d7e18ee9f
s24r87 <s24r87@sbcglobal.net>
2017-05-08 13:37:26 -0400

    Clean out the old google sheet configuration

--- application/dreamsuite/settings/local.py
+++ application/dreamsuite/settings/local.py
@@ -14,17 +14,6 @@ MIDDLEWARE_CLASSES += [
 CORS_ORIGIN_ALLOW_ALL = True
 INSTALLED_APPS.append('debug_toolbar')
 
-
-SPREADSHEET_NAME = 'Tinyhands'
-IRF_WORKSHEET_NAME = 'IRF Entry'
-VIF_WORKSHEET_NAME = 'VIF Entry'
-ADDRESS2_WORKSHEET_NAME = 'Address2 Export'
-
-IMPORT_ACCOUNT_EMAIL = 'test_sup@example.com'
-IMPORT_SPREADSHEET_NAME = 'Tinyhands'
-IRF_IMPORT_WORKSHEET_NAME = 'IRF Import'
-VIF_IMPORT_WORKSHEET_NAME = 'VIF Import'
-
 SPREADSHEET_CONFIG = {
     'IRF': {
         'key_column' : 'IRF Number',
