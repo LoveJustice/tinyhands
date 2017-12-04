995bd49f9987ba765edb56f48928a385976f24f8
s24r87 <s24r87@sbcglobal.net>
2017-12-04 18:07:35 -0500

    Add check that user IsAuthenticated when returning interception alerts.

--- application/dataentry/views/interception_alert.py
+++ application/dataentry/views/interception_alert.py
@@ -1,5 +1,7 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_api.authentication import HasPermission
 
 from dataentry.models import InterceptionAlert
 from dataentry.serializers import InterceptionAlertSerializer
@@ -7,13 +9,14 @@ from dataentry.serializers import InterceptionAlertSerializer
 class InterceptionAlertViewSet(viewsets.ModelViewSet):
     queryset = InterceptionAlert.objects.all()
     serial_class = InterceptionAlertSerializer
+    permission_classes = (IsAuthenticated, HasPermission)
+    permissions_required = []
     
     def get_interception_alerts(self, request):
         latest_id = request.GET.get('latest_id',0)
         print latest_id
         results = InterceptionAlert.objects.filter(id__gt=latest_id).order_by('id')
         print results
-        #serializer = InterceptionAlertSerializer(results, many=True, context={'request': request})
         response_data = '['
         sep =''
         for result in results:
