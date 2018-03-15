b7cb6d65e64b7029f147c154444f2c19558c038a
Ben Duggan <benaduggan@gmail.com>
2018-03-15 17:13:09 -0400

    fixing tests

--- application/dataentry/tests/test_photo_exporter.py
+++ application/dataentry/tests/test_photo_exporter.py
@@ -12,14 +12,14 @@ class PhotoTest(APITestCase):
         self.client.force_authenticate(user=self.user)
 
     def test_photo_count_contains_all_photos(self):
-        url = reverse('PhotoExporterCount', kwargs={"startDate": "8-4-2007", "endDate": "8-4-2013"})
+        url = reverse('PhotoExporterCount', kwargs={"start_date": "8-4-2007", "end_date": "8-4-2013"})
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 20)
 
     def test_photo_count_contains_zero_photos(self):
-        url = reverse('PhotoExporterCount', kwargs={"startDate": "8-4-2014", "endDate": "8-4-2015"})
+        url = reverse('PhotoExporterCount', kwargs={"start_date": "8-4-2014", "end_date": "8-4-2015"})
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -30,12 +30,12 @@ class PhotoTest(APITestCase):
         self.client.force_authenticate(user=self.bad_user)
 
         # get
-        url = reverse('PhotoExporter', kwargs={"startDate": "8-4-2007", "endDate": "8-4-2013"})
+        url = reverse('PhotoExporter', kwargs={"start_date": "8-4-2007", "end_date": "8-4-2013"})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
         # get
-        url = reverse('PhotoExporterCount', kwargs={"startDate": "8-4-2007", "endDate": "8-4-2013"})
+        url = reverse('PhotoExporterCount', kwargs={"start_date": "8-4-2007", "end_date": "8-4-2013"})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
@@ -43,10 +43,10 @@ class PhotoTest(APITestCase):
         self.bad_user = BadIrfUserFactory.create()
         self.client.force_authenticate(user=None)
 
-        url = reverse('PhotoExporter', kwargs={"startDate": "8-4-2007", "endDate": "8-4-2013"})
+        url = reverse('PhotoExporter', kwargs={"start_date": "8-4-2007", "end_date": "8-4-2013"})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-        url = reverse('PhotoExporterCount', kwargs={"startDate": "8-4-2007", "endDate": "8-4-2013"})
+        url = reverse('PhotoExporterCount', kwargs={"start_date": "8-4-2007", "end_date": "8-4-2013"})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
