06ca7ce730ee843c6a0b6e648215e07e1b845afd
s24r87 <s24r87@sbcglobal.net>
2018-09-14 14:04:13 -0400

    Change the way that form data is loaded that no longer relies on migrations.  Add new management command to load the form data.  When form data fixture file is updated, the new version will automatically be loaded when the server is started.

--- application/bin/dump-form-data.sh
+++ application/bin/dump-form-data.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
-if [ $# -ge 1 ]
-then
-	tag=$1
-else
-	tag=$(date "+%Y%m%d")
-fi
+
 tmpFile=/tmp/tmp$$
 tmpFile2=/tmp/tmp2$$
-outFile=fixtures/form_data_$tag.json
-rm -f $tmpFile
+tmpFile3=/tmp/tmp3$$
+outFile=fixtures/initial-required-data/form_data.json
+rm -f $tmpFile $tmpFile2 $tmpFile3
 
 for model in FormType Storage Form CategoryType Category CardStorage AnswerType Question QuestionLayout QuestionStorage Answer FormValidationLevel FormValidationType FormValidation FormValidationQuestion ExportImport ExportImportCard ExportImportField GoogleSheetConfig
 do
 	./manage.py dumpdata dataentry.$model >> $tmpFile
 done
 sed -e 's/\]\[/,/g' < $tmpFile > $tmpFile2
-python /data/bin/remove_station_reference.py $tmpFile2 $outFile
+python /data/bin/remove_station_reference.py $tmpFile2 $tmpFile3
+python -m json.tool < $tmpFile3 > $outFile
+rm -f $tmpFile $tmpFile2 $tmpFile3
