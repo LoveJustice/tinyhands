e77d3b67b2c0f2c9b0855c14fd99c83a538b5797
scrishel <scrishel@sbcglobal.net>
2018-12-15 12:44:23 -0500

    Another codacy issue

--- application/dataentry/views/person.py
+++ application/dataentry/views/person.py
@@ -2,7 +2,7 @@ from rest_framework import filters as fs
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_api.authentication import HasPermission, HasDeletePermission, HasPostPermission
+from rest_api.authentication import HasPermission
 
 from dataentry.models import CardStorage, Form, Person, Question
 from dataentry.serializers import PersonSerializer
