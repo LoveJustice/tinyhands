f2622844cac095d61554abaa8777aa079a6984be
scrishel <scrishel@sbcglobal.net>
2019-10-15 17:21:05 -0400

    Update indicatorHistory to build history for all or individual month

--- application/dataentry/management/commands/indicatorHistory.py
+++ application/dataentry/management/commands/indicatorHistory.py
@@ -6,17 +6,39 @@ from django.core.management.base import BaseCommand
 from dataentry.models import Country, IndicatorHistory
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        countries = Country.objects.all()
-        now = datetime.datetime.now()
-        end_year = now.year
-        end_month = now.month
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--all',
+            action='store_true',
+            help='Remove eisting indicator history if present and calculate indicators for all countries',
+        )
         
-        start_year = 2017
-        start_month = 9
+    def handle(self, *args, **options):
+        now = datetime.datetime.now()
+        if now.month == 1:
+            end_year = now.year - 1
+            end_month = 12
+        else:
+            end_year = now.year
+            end_month = now.month - 1
+        
+        if options['all']:
+            print ('Calculating idicators for all countries from 2017-9 through ' + str(end_year) + '-' + str(end_month))
+            IndicatorHistory.objects.all().delete()
+            start_year = 2017
+            start_month = 9
+            always_store_indicators = False
+        else:
+            print ('Calculating idicators for all countries for the month ' + str(end_year) + '-' + str(end_month))
+            IndicatorHistory.objects.filter(year=end_year, month=end_month).delete()
+            start_year = end_year
+            start_month = end_month
+            always_store_indicators = True
+
+        countries = Country.objects.all()
         
         for country in countries:
-            found_indicators = False
+            found_indicators = always_store_indicators
             for year in range(start_year, end_year+1):
                 if year == start_year:
                     first_month = start_month
@@ -26,9 +48,9 @@ class Command(BaseCommand):
                 if year == end_year:
                     last_month = end_month
                 else:
-                    last_month = 13
+                    last_month = 12
                 
-                for month in range(first_month, last_month):
+                for month in range(first_month, last_month+1):
                     start_date = datetime.date(year, month, 1)
                     if month == 12:
                         end_date = datetime.date(year+1, 1, 1) - datetime.timedelta(1)
@@ -41,7 +63,7 @@ class Command(BaseCommand):
                         ('vdfCount' not in results or results['vdfCount'] ==0) and
                         ('photosCount' not in results or results['photosCount'] ==0)):
                         continue
-                    print (country.name, start_date, end_date, results)
+                    #print (country.name, start_date, end_date, results)
                     found_indicators = True
                     
                     indicator = IndicatorHistory()
