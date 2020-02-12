3e976bcc4a61a1fccd03fb84df0b31a9bdddeff6
scrishel <s24r87@sbcglobal.net>
2020-02-12 08:46:20 -0500

    Remove old management commands that reference old models.

--- application/dataentry/management/commands/importIrf.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from export_import.google_sheet_import import GoogleSheetImport
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        GoogleSheetImport.import_data('IRF')
