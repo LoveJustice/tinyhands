9958369e0eba2af17eb75c86351d16f342b98e05
Austin <amunn33@gmail.com>
2020-03-14 21:00:16 +0545

    Update 0134_auto_20200311_1355.py
    Fix dependency issue

--- application/dataentry/migrations/0134_auto_20200311_1355.py
+++ application/dataentry/migrations/0134_auto_20200311_1355.py
@@ -10,7 +10,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dataentry', '0133_auto_20200219_1854'),
+        ('dataentry', '0133_auto_20200224_1828'),
     ]
 
     operations = [
