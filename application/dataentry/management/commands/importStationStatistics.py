d2e4904941c5c3b5f3ddd4a8b080b1d4ce59c239
scrishel <scrishel@sbcglobal.net>
2020-08-19 12:24:08 -0400

    Updates to statistics import management command

--- application/dataentry/management/commands/importStationStatistics.py
+++ application/dataentry/management/commands/importStationStatistics.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
+from django.db.models import Sum
 
 from dataentry.models import BorderStation, StationStatistics, LocationStatistics, LocationStaff
 from static_border_stations.models import Location, Staff
@@ -13,6 +14,7 @@ class Command(BaseCommand):
         parser.add_argument('filename', nargs='+', type=str)
         parser.add_argument('startYearMonth', nargs='+', type=int)
         parser.add_argument('endYearMonth', nargs='+', type=int)
+        parser.add_argument('--fix', action='append', type=str)
     def handle(self, *args, **options):
         mode = options.get('mode')[0]
         file_name = options.get('filename')[0]
@@ -38,21 +40,87 @@ class Command(BaseCommand):
             'Staff':'work_fraction'
             }
         
-        if (mode == 'station_pre'):
-            self.processStationStatistics(file_name, start_year, start_month, end_year, end_month, True)
-        elif (mode == 'station_post'):
-            self.processStationStatistics(file_name, start_year, start_month, end_year, end_month, False)
-        elif (mode == 'location'):
-            self.processLocationStatistics(file_name, start_year, start_month, end_year, end_month);
+        fixes = {
+            'mapping': {},
+            'new_location': [],
+            'rename': {},
+            'not_monitoring': {}
+            }
+        if options['fix']:
+            self.process_fixes(options['fix'][0], fixes)
         
+        if (mode == 'station_pre'):
+            self.process_station_statistics(file_name, start_year, start_month, end_year, end_month, True)
+        elif (mode == 'station_post'):
+            self.process_station_statistics(file_name, start_year, start_month, end_year, end_month, False)
+        elif (mode == 'location'):
+            self.processLocationStatistics(file_name, start_year, start_month, end_year, end_month, fixes['mapping'])
+        elif (mode == 'arrest'):
+            self.processArrests(file_name)
     
-    def processLocationStatistics(self, file_name, start_year, start_month, end_year, end_month):
+    def process_fixes(self, fix_file, fixes):
+        with open(fix_file) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['Station1']  != '':
+                    if row['New Location'] == 'TRUE':
+                        fixes['new_location'].append({'station':row['Station1'], 'location':row['Location']})
+                    elif row['Matching SL Name'] == '':
+                        print('__Other for', row['Location'])
+                        fixes['mapping'][row['Location']] = "__Other"
+                    else:
+                        fixes['mapping'][row['Location']] = row['Matching SL Name']
+                
+                if row['New Location Name'] != '':
+                    fixes['rename'][row['Current Location']] = {'station':row['Station2'], 'new_location':row['New Location Name']}
+                
+                if row['Currently Monitoring'] == 'FALSE':
+                    fixes['not_monitoring'][row['Current Location']] = row['Station2']
+            
+            for new_location in fixes['new_location']:
+                try:
+                    existing = Location.objects.get(border_station__station_name=new_location['station'], name=new_location['location'])
+                    print('location', new_location['location'], 'already exists for station', new_location['station'])
+                except ObjectDoesNotExist:
+                    location = Location()
+                    print('new_location, border_station', new_location['station'])
+                    location.border_station = BorderStation.objects.get(station_name=new_location['station'])
+                    location.name = new_location['location']
+                    location.save()
+            
+            for key, value in fixes['rename'].items():
+                try:
+                    location = Location.objects.get(border_station__station_name=value['station'], name=key)
+                    location.name = value['new_location']
+                    location.save()
+                except ObjectDoesNotExist:
+                    print('rename location', key, value, 'not found')
+            
+            for key, value in fixes['not_monitoring'].items():
+                try:
+                    location = Location.objects.get(border_station__station_name = value, name=key)
+                except ObjectDoesNotExist:
+                    print ('Unable to find location for station',value,'location', key)
+                
+                location.active = False
+                location.save()
+            
+            for key, value in fixes['mapping'].items():
+                if value in fixes['rename']:
+                    fixes['mapping'][key] = fixes['rename'][value]['new_location']
+                    
+            #print("MAPPING", fixes['mapping'])
+                    
+                    
+    def processLocationStatistics(self, file_name, start_year, start_month, end_year, end_month, mapping):
         skip = False
         with open(file_name) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 tmp_station_code = row['Station Code']
-                tmp_location = row['Location']
+                tmp_location = row['Location'].strip()
+                if tmp_location in mapping:
+                    tmp_location = mapping[tmp_location]
                 if tmp_location == 'TOTAL' or tmp_location == '':
                     continue
                 if tmp_location != '' and tmp_location != 'TOTAL' and len(tmp_station_code) == 3:
@@ -68,9 +136,12 @@ class Command(BaseCommand):
                     continue
                 
                 try:
-                    location = Location.objects.get(border_station=station, name__iexact=tmp_location)
+                    if tmp_location == '__Other':
+                        location = Location.get_or_create_other_location(station)
+                    else:
+                        location = Location.objects.get(border_station=station, name__iexact=tmp_location)
                 except ObjectDoesNotExist:
-                    print('Unable to find location with name', tmp_location)
+                    print('Unable to find location with name', '"' + tmp_location + '"')
                     continue
             
                 month = start_month
@@ -92,13 +163,18 @@ class Command(BaseCommand):
                         entry.year_month = year_month
                     
                     modified = False
-                    for key in self.location_map.keys():
+                    for key in ['Int']:
                         if key + year_month_csv in row:
                             value = row[key + year_month_csv]
                             value = value.replace(',','')
+                            old_value = getattr(entry, self.location_map[key])
+                            if old_value is None:
+                                old_value = 0
                             if value != '':
-                                setattr(entry, self.location_map[key], value)
+                                setattr(entry, self.location_map[key], old_value + int(value))
+                                #print (station.station_code, location.name, key + year_month_csv, old_value, value, entry.id)
                                 modified = True
+                        
                     if modified:
                             entry.save()
                     
@@ -128,7 +204,7 @@ class Command(BaseCommand):
                         month = 1
                         
                         
-    def processStationStatistics(self, file_name, start_year, start_month, end_year, end_month, include_arrests_and_staff):
+    def process_station_statistics(self, file_name, start_year, start_month, end_year, end_month, include_arrests_and_staff):
         with open(file_name) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
@@ -176,7 +252,7 @@ class Command(BaseCommand):
                             entry.year_month = year_month
                         
                         modified = False
-                        for key in self.location_map.keys():
+                        for key in ['Int']:
                             if key + year_month_csv in row:
                                 value = row[key + year_month_csv]
                                 value = value.replace(',','')
@@ -210,8 +286,51 @@ class Command(BaseCommand):
                     if month > 12:
                         year += 1
                         month = 1
+    
+    def processArrest(self, station_name, arrest_date, results):
+        try:
+            arrest_parts = arrest_date.split('/')
+            year_month = 100 * int(arrest_parts[2]) + int(arrest_parts[0])
+        except:
+            print ('failed to get year_month. Station', station_name, 'date', arrest_date)
+            return
+        
+        if station_name not in results:
+            results[station_name] = {}
+        
+        if year_month in results[station_name]:
+            results[station_name][year_month] += 1
+        else:
+            results[station_name][year_month] = 1
+        
+    def processArrests (self, file_name):
+        results = {}
+        with open(file_name) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['Closed Station'] != '' and row['Closed Arrested'] != '':
+                    self.processArrest(row['Closed Station'], row['Closed Arrested'], results)
                 
+                if row['Inactive Station'] != '' and row['Inactive Arrested'] != '':
+                    self.processArrest(row['Inactive Station'], row['Inactive Arrested'], results)
                 
-                
+                if row['Active Station'] != '' and row['Active Arrested'] != '':
+                    self.processArrest(row['Active Station'], row['Active Arrested'], results)
+        
+        for key in results.keys():
+            try:
+                station = BorderStation.objects.get(station_name=key)
+            except ObjectDoesNotExist:
+                print ("Unable to find station with name", key)
+                continue
             
-            
+            for year_month in results[key].keys():
+                location_total = LocationStatistics.objects.filter(location__border_station=station, year_month=year_month).aggregate(Sum('arrests'))['arrests__sum']
+                if location_total is None:
+                    location_total = 0
+                
+                if location_total > results[key][year_month]:
+                    print (key, year_month, location_total, results[key][year_month], "********")
+                else:
+                    print (key, year_month, location_total, results[key][year_month])
+                    
