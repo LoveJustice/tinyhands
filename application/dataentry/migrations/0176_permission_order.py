d66d49d5e8b3bc3c6fcf821681315fb1030a8642
scrishel <s24r87@sbcglobal.net>
2021-03-30 17:46:17 -0400

    Fix migration issue after merge with develop

--- application/dataentry/migrations/0176_permission_order.py
+++ application/dataentry/migrations/0176_permission_order.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dataentry', '0175_clientdiagnostic'),
+        ('dataentry', '0176_clientdiagnostic'),
     ]
 
     operations = [
