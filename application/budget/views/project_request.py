3780575a5848320459437bc0214217db126dec31
scrishel <s24r87@sbcglobal.net>
2024-02-07 15:06:29 -0500

    Update templated email for discussion notification

--- application/budget/views/project_request.py
+++ application/budget/views/project_request.py
@@ -302,12 +302,24 @@ class ProjectRequestDiscussionViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_400_BAD_REQUEST)
         
         discussion = serializer.save()
+        
+        category_name = ''
+        for category in constants.CATEGORY_CHOICES:
+            if category[0] == discussion.request.category:
+                category_name = category[1]
+                
         for account_id in request.data['notify']:
             account = Account.objects.get(id=account_id)
             discussion.notify.add(account)
         
             context = {}
+            context['staff'] = account.get_full_name()
+            context['Comment_user'] = request.user.get_full_name()
             context['url'] = settings.CLIENT_DOMAIN +'/reviewProjectRequests?id=' + str(discussion.request.id)
+            context['Project'] = discussion.request.project.station_name
+            context['Category'] = category_name
+            context['Description'] = discussion.request.description
+            context['Comment'] = discussion.text
             send_templated_mail(
                 template_name='discussion_notify',
                 from_email=settings.ADMIN_EMAIL_SENDER,
