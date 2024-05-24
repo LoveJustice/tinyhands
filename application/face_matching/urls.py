e453645021d73f79b1501b041bbc882af8cab1bd
Demeaus Minnihan <demeaus.wong@gmail.com>
2024-05-23 22:20:54 -0600

    Removed more unused files. Removed __pycache__ from .gitignore.

--- application/face_matching/urls.py
+++ application/face_matching/urls.py
@@ -3,12 +3,6 @@ from .views import FaceMatchingViewSet
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
-    # path('compare/', views.compare_photos, name='compare_photos'),
-    # path('select/', views.select_from_records, name='select_from_records'),
-    # path('upload/', views.upload_photo, name='upload_photo'),
-    # path('take/', views.take_photo, name='take_photo'),
-
     #  Get matches for encoded 
     re_path(r'^face-matching/upload/$', FaceMatchingViewSet.as_view({'post': 'get_upload_matches'}), name='FaceMatchesList'),
 ]
