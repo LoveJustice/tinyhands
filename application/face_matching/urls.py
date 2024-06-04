58b691a6e0644efb7c11b13906e1e94004279e9a
Demeaus Minnihan <demeaus.wong@gmail.com>
2024-06-04 12:53:19 -0600

    Replaced redefined models with imports.

--- application/face_matching/urls.py
+++ application/face_matching/urls.py
@@ -3,6 +3,5 @@ from .views import FaceMatchingViewSet
 from . import views
 
 urlpatterns = [
-    #  Get matches for encoded 
     re_path(r'^face-matching/upload/$', FaceMatchingViewSet.as_view({'post': 'get_upload_matches'}), name='FaceMatchesList'),
 ]
