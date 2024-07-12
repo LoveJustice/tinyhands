7556fb128fb6aee0470d945cbfc8572fb2747041
scrishel <s24r87@sbcglobal.net>
2024-07-12 15:22:22 -0400

    Add missing form_log.py file

---
+++ application/dataentry/models/form_log.py
@@ -0,0 +1,10 @@
+from django.db import models
+from accounts.models import Account
+
+class FormLog(models.Model):
+	date_time = models.DateTimeField(auto_now_add=True)
+	performed_by = models.ForeignKey(Account, null=True, on_delete=models.SET_NULL)
+	form_name = models.CharField(max_length=126)
+	form_number = models.CharField(max_length=126)
+	form_id = models.IntegerField()
+	action = models.CharField(max_length=126)
